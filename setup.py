9fbcb33089b20003e042c2f6210a68d2121913ee
Colton Myers <colton.myers@gmail.com>
2017-01-18 15:32:08 -0700

    Add platform detection for centos 6/7

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import setup, find_packages
+import platform
+
+distro, version, _ = platform.dist()
+
+# Default to cent7
+data_files = [('/usr/lib/systemd/system', ['pkg/hubble.service']),]
+
+if distro == 'redhat' or distro == 'centos':
+    if version.startswith('6'):
+        data_files = [('/etc/init.d', ['pkg/hubble']),]
+    elif version.startswith('7'):
+        data_files = [('/usr/lib/systemd/system', ['pkg/hubble.service']),]
+
 setup(
     name="hubblestack",
     version="2.0",
@@ -11,7 +24,7 @@ setup(
     install_requires=[
         'salt >= 2016.3.4',
     ],
-    data_files=[('/etc/init.d', ['pkg/hubble']),],
+    data_files=data_files,
     options={
 #        'build_scripts': {
 #            'executable': '/usr/bin/env python',

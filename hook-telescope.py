7ddb82d807e3f2dbe4cd7bf7d4b88d8d63631b2b
root <root@ip-172-31-5-221.us-west-1.compute.internal>
2017-01-25 07:09:36 +0000

    Added new hooks, telescope exec and pyinstaller file

---
+++ hook-telescope.py
@@ -0,0 +1,27 @@
+HIDDEN_IMPORTS = [
+ 'BaseHTTPServer',
+ 'argparse',
+ 'logging',
+ 'time',
+ 'pprint',
+ 'os',
+ 'random',
+ 'signal',
+ 'sys',
+ 'git',
+ 'daemon',
+ 'boto3',
+ 'botocore',
+ 'imp',
+ 'six',
+ 'inspect',
+ 'yaml',
+ 'traceback'
+]
+DATAS = []
+binaries = []
+
+
+hiddenimports = HIDDEN_IMPORTS
+#datas = DATAS
+#binaries = BINARIES

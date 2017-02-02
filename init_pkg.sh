9f43fddf7bc9163bb67e0ed32d4ba6d19bfc20ce
ec2-user <ec2-user@ip-172-31-22-68.us-west-1.compute.internal>
2017-02-03 07:06:36 +0900

    Removed python 2.6.6 support

--- init_pkg.sh
+++ init_pkg.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Installing minimum of python 2.7
+_python_version=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
+
+read -r -d "" _PYTHON_HELP <<EOF
+
+You are running an older version python ... \n \n
+
+Please run the following commands to setup python27 \n \n
+
+sudo yum update \n
+sudo yum install centos-release-scl \n
+sudo yum install python27 \n
+scl enable python27 bash \n
+
+EOF
+
+if [ "$_python_version" == "2.6.6" ]
+then
+  echo -e $_PYTHON_HELP
+  exit
+fi
+
+
 cd scripts/
 sudo bash installessentials.sh
 sudo bash libgit2-build.sh
--- scripts/pip-install.sh
+++ scripts/pip-install.sh
@@ -1,9 +1,1 @@
-_python_version=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
-
-if [ "$_python_version" == "2.6.6" ]
-then
-  pip install -r pyinstaller-requirements-2.6.txt
-  pip uninstall jinja2
-else
   pip install -r pyinstaller-requirements.txt
-fi

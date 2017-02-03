a4126436f5a8be2b03c0a59856b9790c114fffe7
Cloud User <centos@ip-172-31-8-203.us-west-1.compute.internal>
2017-02-03 20:04:06 +0000

    Check for root user run and exit the script, if the script is run as root user.

--- init_pkg.sh
+++ init_pkg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+_user=`id -u`
+
 # Installing minimum of python 2.7
 _python_version=`python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))'`
 
@@ -16,12 +18,21 @@ scl enable python27 bash \n
 
 EOF
 
+# Check if the python version is 2.6.6
 if [ "$_python_version" == "2.6.6" ]
 then
   echo -e $_PYTHON_HELP
   exit
 fi
 
+# Check if the current user is root
+if [ "$_user" == "0" ]
+then
+  echo "This script should not be run as root ..."
+  echo "Please run this script as regular user with sudo privileges ..."
+  echo "Exiting ..."
+  exit
+fi
 
 cd scripts/
 sudo bash installessentials.sh

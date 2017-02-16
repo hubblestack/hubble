35f5284a96309a92d7cd068f310a3c2bb55617e9
Colton Myers <colton.myers@gmail.com>
2017-02-16 14:50:26 -0700

    Add uptime fallback query

--- hubblestack/extmods/modules/nebula_osquery.py
+++ hubblestack/extmods/modules/nebula_osquery.py
@@ -99,6 +99,15 @@ def queries(query_group,
                              'result': True
                         }}
                 )
+            uptime = __salt__['status.uptime']()
+            if isinstance(uptime, dict):
+                uptime = uptime.get('seconds', __salt__['cmd.run']('uptime'))
+            ret.append(
+                    {'fallback_uptime': {
+                         'data': [{'uptime': uptime}],
+                         'result': True
+                    }}
+            )
             if report_version_with_day:
                 ret.append(hubble_versions())
             return ret

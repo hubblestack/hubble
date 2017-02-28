06b01af115e000965b2e3a06aafcece531005b8f
Colton Myers <colton.myers@gmail.com>
2017-02-28 12:30:33 -0700

    Rev to 2.1.3

--- pkg/build_debs.sh
+++ pkg/build_debs.sh
@@ -17,16 +17,16 @@ cd ..
 
 bash init_pkg.sh -y
 cp hubble.tar.gz ~/hubble.tar.gz
-rm -rf ~/hubblestack-2.1.2
-rm -rf ~/hubblestack-2.1.2.tar.gz
-mkdir ~/hubblestack-2.1.2
-tar -xzvf ~/hubble.tar.gz -C ~/hubblestack-2.1.2
-mkdir -p ~/hubblestack-2.1.2/etc/init.d
-cp pkg/hubble ~/hubblestack-2.1.2/etc/init.d
-mkdir -p ~/hubblestack-2.1.2/usr/lib/systemd/system
-cp pkg/hubble.service ~/hubblestack-2.1.2/usr/lib/systemd/system
-cp -f conf/hubble ~/hubblestack-2.1.2/etc/hubble/hubble
-cd ~/hubblestack-2.1.2
+rm -rf ~/hubblestack-2.1.3
+rm -rf ~/hubblestack-2.1.3.tar.gz
+mkdir ~/hubblestack-2.1.3
+tar -xzvf ~/hubble.tar.gz -C ~/hubblestack-2.1.3
+mkdir -p ~/hubblestack-2.1.3/etc/init.d
+cp pkg/hubble ~/hubblestack-2.1.3/etc/init.d
+mkdir -p ~/hubblestack-2.1.3/usr/lib/systemd/system
+cp pkg/hubble.service ~/hubblestack-2.1.3/usr/lib/systemd/system
+cp -f conf/hubble ~/hubblestack-2.1.3/etc/hubble/hubble
+cd ~/hubblestack-2.1.3
 
 sudo apt-get install -y ruby ruby-dev rubygems gcc make
 sudo gem install --no-ri --no-rdoc fpm
@@ -36,9 +36,9 @@ ln -s /opt/osquery/osqueryd usr/bin/osqueryd
 ln -s /opt/osquery/osqueryi usr/bin/osqueryi
 fpm -s dir -t deb \
     -n hubblestack \
-    -v 2.1.2-1 \
+    -v 2.1.3-1 \
     -d 'git' \
     --config-files /etc/hubble/hubble --config-files /etc/osquery/osquery.conf \
     --deb-no-default-config-files \
     etc/hubble etc/osquery etc/init.d opt usr/bin
-cp hubblestack_2.1.2-1_amd64.deb ~/
+cp hubblestack_2.1.3-1_amd64.deb ~/

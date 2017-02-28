ea12b6a2f9eddf26aa156bd1d417c009b618588d
Colton Myers <colton.myers@gmail.com>
2017-02-28 12:15:49 -0700

    Add debian-building script

---
+++ pkg/build_debs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x # echo on
+
+_user=`id -u`
+
+# Check if the current user is root
+if [ "$_user" == "0" ]
+then
+  echo "This script should not be run as root ..."
+  echo "Please run this script as regular user with sudo privileges ..."
+  echo "Exiting ..."
+  exit
+fi
+
+cd ..
+
+bash init_pkg.sh -y
+cp hubble.tar.gz ~/hubble.tar.gz
+rm -rf ~/hubblestack-2.1.2
+rm -rf ~/hubblestack-2.1.2.tar.gz
+mkdir ~/hubblestack-2.1.2
+tar -xzvf ~/hubble.tar.gz -C ~/hubblestack-2.1.2
+mkdir -p ~/hubblestack-2.1.2/etc/init.d
+cp pkg/hubble ~/hubblestack-2.1.2/etc/init.d
+mkdir -p ~/hubblestack-2.1.2/usr/lib/systemd/system
+cp pkg/hubble.service ~/hubblestack-2.1.2/usr/lib/systemd/system
+cp -f conf/hubble ~/hubblestack-2.1.2/etc/hubble/hubble
+cd ~/hubblestack-2.1.2
+
+sudo apt-get install -y ruby ruby-dev rubygems gcc make
+sudo gem install --no-ri --no-rdoc fpm
+mkdir -p usr/bin
+ln -s /opt/hubble/hubble usr/bin/hubble
+ln -s /opt/osquery/osqueryd usr/bin/osqueryd
+ln -s /opt/osquery/osqueryi usr/bin/osqueryi
+fpm -s dir -t deb \
+    -n hubblestack \
+    -v 2.1.2-1 \
+    -d 'git' \
+    --config-files /etc/hubble/hubble --config-files /etc/osquery/osquery.conf \
+    --deb-no-default-config-files \
+    etc/hubble etc/osquery etc/init.d opt usr/bin
+cp hubblestack_2.1.2-1_amd64.deb ~/

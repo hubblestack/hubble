984a1773c666375d87d078af9e09fb80f4bc32c8
Colton Myers <colton.myers@gmail.com>
2017-03-08 14:59:54 -0700

    Move pkg building stuff into build/ dir, make build_rpms.sh more portable

---
+++ build/build_rpms.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -x # echo on
+
+_user=`id -u`
+
+# Check if the current user is root
+if [ "$_user" == "0" ]
+then
+  echo "This script should not be run as root ..."
+  echo "Please run this script as regular user with sudo privileges ..."
+  echo "Exiting ..."
+  exit
+fi
+
+rm -rf build
+rm -rf dist
+
+mkdir -p build
+mkdir -p dist
+
+bash ./init_pkg.sh -y
+cp hubble.tar.gz dist/hubble.tar.gz
+mv hubble.tar.gz build/hubble.tar.gz
+mkdir build/hubblestack-2.1.4
+tar -xzvf build/hubble.tar.gz -C build/hubblestack-2.1.4
+mkdir -p build/hubblestack-2.1.4/etc/init.d
+cp ../pkg/hubble build/hubblestack-2.1.4/etc/init.d
+mkdir -p build/hubblestack-2.1.4/usr/lib/systemd/system
+cp ../pkg/hubble.service build/hubblestack-2.1.4/usr/lib/systemd/system
+cp -f ../conf/hubble build/hubblestack-2.1.4/etc/hubble/hubble
+cd build
+tar -czvf hubblestack-2.1.4.tar.gz hubblestack-2.1.4/
+mkdir -p rpmbuild/{RPMS,SRPMS,BUILD,SOURCES,SPECS,tmp}
+
+cat <<EOF >.rpmmacros
+%_topdir   %(pwd)/rpmbuild
+%_tmppath  %{_topdir}/tmp
+EOF
+
+cp hubblestack-2.1.4.tar.gz rpmbuild/SOURCES/
+cd rpmbuild
+
+cp ../../specs/* SPECS/
+
+rpmbuild -ba SPECS/hubblestack-el6.spec
+mkdir -p ../../dist/el6
+cp RPMS/x86_64/* ../../dist/el6/
+rpmbuild -ba SPECS/hubblestack-el7.spec
+mkdir -p ../../dist/el7
+cp RPMS/x86_64/* ../../dist/el7/

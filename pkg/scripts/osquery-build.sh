ba3a7252f92cf757aecd625f97a927d72908f70a
Colton Myers <colton.myers@gmail.com>
2017-04-04 15:42:11 -0600

    Add ncurses6 build for debian 7

--- pkg/scripts/osquery-build.sh
+++ pkg/scripts/osquery-build.sh
@@ -2,6 +2,15 @@
 mkdir -p temp
 sudo chown -R $USER. temp
 cd temp
+if [[ -n "$(python -mplatform | grep debian-7)" && -f ./ncurses-6.0/install/lib/libncursesw.so.6.0 ]]; then
+    wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
+    tar -xzvf ncurses-6.0.tar.gz
+    cd ncurses-6.0
+    ./configure --prefix=$(pwd)/install --with-shared --enable-widec
+    make
+    make install
+    export LD_LIBRARY_PATH=$(pwd)/install/lib
+fi
 git clone https://github.com/facebook/osquery.git
 cd osquery
 git checkout 2.3.2

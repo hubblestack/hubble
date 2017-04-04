f2cbe5c6775ae0c057caf840fdde604c93e9470b
Colton Myers <colton.myers@gmail.com>
2017-04-04 16:57:22 -0600

    Do it the easy way

--- pkg/scripts/osquery-build.sh
+++ pkg/scripts/osquery-build.sh
@@ -2,21 +2,15 @@
 mkdir -p temp
 sudo chown -R $USER. temp
 cd temp
-if [[ -n "$(python -mplatform | grep debian-7)" && ! -f ./ncurses-6.0/install/lib/libncursesw.so.6.0 ]]; then
-    wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
-    tar -xzvf ncurses-6.0.tar.gz
-    pushd ncurses-6.0
-    ./configure --prefix=$(pwd)/install --with-shared --enable-widec
-    make
-    make install
-    export LD_LIBRARY_PATH=$(pwd)/install/lib
-    popd
-fi
 git clone https://github.com/facebook/osquery.git
 cd osquery
 git checkout 2.3.2
 make sysprep
 make deps
+if [[ -n "$(python -mplatform | grep debian-7)" ]]; then
+    /usr/local/osquery/bin/brew untap homebrew/dupes
+    /usr/local/osquery/bin/brew link ncurses
+fi
 SKIP_TESTS=1 make -j 4
 make strip
 sudo cp -pr ./build/linux/osquery/osqueryi ./build/linux/osquery/osqueryd /opt/osquery

c8b915ec84508e1259526ff0ea2461d0b1104b07
Colton Myers <colton.myers@gmail.com>
2017-07-31 12:40:09 -0600

    Add pulsar.canary to win_pulsar as well

--- hubblestack/extmods/modules/win_pulsar.py
+++ hubblestack/extmods/modules/win_pulsar.py
@@ -221,6 +221,21 @@ def process(configfile='salt://hubblestack_pulsar/hubblestack_pulsar_win_config.
     return ret
 
 
+def canary(change_file=None):
+    '''
+    Simple module to change a file to trigger a FIM event (daily, etc)
+
+    THE SPECIFIED FILE WILL BE CREATED AND DELETED
+
+    Defaults to CONF_DIR/fim_canary.tmp, i.e. /etc/hubble/fim_canary.tmp
+    '''
+    if change_file is None:
+        conf_dir = os.path.dirname(__opts__['conf_file'])
+        change_file = os.path.join(conf_dir, 'fim_canary.tmp')
+    __salt__['file.touch'](change_file)
+    __salt__['file.remove'](change_file)
+
+
 def _check_acl(path, mask, wtype, recurse):
     audit_dict = {}
     success = True

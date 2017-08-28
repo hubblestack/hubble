bd59054d8526144042d014ebecf84aae0a0e3b79
Chad Mills <chadlmills@gmail.com>
2017-08-28 09:38:05 -0600

    added logging when path specied in yaml doesnt exist.  Changed blacklist from OR to list

--- hubblestack/extmods/modules/win_pulsar.py
+++ hubblestack/extmods/modules/win_pulsar.py
@@ -28,6 +28,8 @@ __virtualname__ = 'pulsar'
 CONFIG = None
 CONFIG_STALENESS = 0
 
+__version__ = 'v2017.8.3'
+
 
 def __virtual__():
     if not salt.utils.is_windows():
@@ -156,9 +158,10 @@ def process(configfile='salt://hubblestack_pulsar/hubblestack_pulsar_win_config.
     # Validate ACLs on watched folders/files and add if needed
     if update_acls:
         for path in config:
-            if path == 'win_notify_interval' or path == 'return' or path == 'batch' or path == 'checksum' or path == 'stats':
+            if path in ['win_notify_interval', 'return', 'batch', 'checksum', 'stats', 'paths', 'verbose']:
                 continue
             if not os.path.exists(path):
+                log.info('The folder path {0} does not exist'.format(path))
                 continue
             if isinstance(config[path], dict):
                 mask = config[path].get('mask', DEFAULT_MASK)
@@ -194,6 +197,9 @@ def process(configfile='salt://hubblestack_pulsar/hubblestack_pulsar_win_config.
     for r in ret:
         _append = True
         config_found = False
+        config_path = config['paths'][0]
+        pulsar_config = config_path[config_path.rfind('/')+1:len(config_path)]
+        r['pulsar_config'] = pulsar_config
         for path in config:
             if not r['Object Name'].startswith(path):
                 continue
@@ -395,9 +401,9 @@ def _remove_acl(path):
 
 def _pull_events(time_frame, checksum):
     events_list = []
-    events_output = __salt__['cmd.run_stdout']('mode con:cols=1000 lines=1000; Get-EventLog -LogName Security '
-                                               '-After ((Get-Date).AddSeconds(-{0})) -InstanceId 4663 | fl'.format(
-                                                time_frame), shell='powershell', python_shell=True)
+    events_output = __salt__['cmd.run_stdout']('mode con:cols=1000 lines=1000; Get-WinEvent -FilterHashTable @{{'
+                                               'LogName = "security"; StartTime = [datetime]::Now.AddSeconds(-30);'
+                                               'Id = 4663}} | fl'.format(time_frame), shell='powershell', python_shell=True)
     events = events_output.split('\r\n\r\n')
     for event in events:
         if event:
@@ -408,12 +414,12 @@ def _pull_events(time_frame, checksum):
                     item.replace('\t', '')
                     k, v = item.split(':', 1)
                     event_dict[k.strip()] = v.strip()
-            event_dict['Accesses'] = _get_access_translation(event_dict['Accesses'])
+            #event_dict['Accesses'] = _get_access_translation(event_dict['Accesses'])
             event_dict['Hash'] = _get_item_hash(event_dict['Object Name'], checksum)
             #needs hostname, checksum, filepath, time stamp, action taken
             # Generate the dictionary without a dictionary comp, for py2.6
             tmpdict = {}
-            for k in ('EntryType', 'Accesses', 'TimeGenerated', 'Object Name', 'Hash'):
+            for k in ('Message', 'Accesses', 'TimeCreated', 'Object Name', 'Hash'):
                 tmpdict[k] = event_dict[k]
             events_list.append(tmpdict)
     return events_list

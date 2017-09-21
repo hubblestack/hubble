8800c949a8565bc96f23892c66121e5cb3609ca8
Chandler Newby <chandler.newby@gmail.com>
2017-09-20 18:22:15 -0600

    Update splunkindex grain

--- hubblestack/extmods/grains/splunkindex.py
+++ hubblestack/extmods/grains/splunkindex.py
@@ -2,6 +2,9 @@
 
 import salt.modules.config
 
+salt.modules.config.__pillar__ = {}
+salt.modules.config.__grains__ = {}
+
 __salt__ = {'config.get': salt.modules.config.get}
 
 
@@ -11,6 +14,7 @@ def splunkindex():
     '''
     # Provides:
     #   splunkindex
+    salt.modules.config.__opts__ = __opts__
     grains = {}
     index = __salt__['config.get']('hubblestack:returner:splunk:index', default='unknown')
     grains['splunkindex'] = index

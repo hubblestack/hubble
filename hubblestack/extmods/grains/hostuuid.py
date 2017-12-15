b2eca79c841cf131efdf08c47548425da831d5dc
Colton Myers <colton.myers@gmail.com>
2017-12-15 12:56:11 -0700

    Add more error checking and back-checking for host_uuid
    https://github.com/hubblestack/hubble/pull/260#pullrequestreview-83715509

--- hubblestack/extmods/grains/hostuuid.py
+++ hubblestack/extmods/grains/hostuuid.py
@@ -15,14 +15,36 @@ def host_uuid():
     Generate a unique uuid for this host, storing it on disk so it persists
     across restarts
     '''
-    cached_uuid = os.path.join(os.path.dirname(__opts__['configfile']), 'hubble_cached_uuid')
+    cached_uuid_path = os.path.join(os.path.dirname(__opts__['configfile']), 'hubble_cached_uuid')
+    existing_uuid = __opts__.get('host_uuid', None)
     try:
-        if os.path.isfile(cached_uuid):
-            with open(cached_uuid, 'r') as f:
-                return {'host_uuid': f.read()}
-    except Exception as exc:
-        log.exception('Problem retrieving cached host uuid')
-    generated = str(uuid.uuid4())
-    with open(cached_uuid, 'w') as f:
-        f.write(generated)
-    return {'host_uuid': generated}
+        if os.path.isfile(cached_uuid_path):
+            with open(cached_uuid_path, 'r') as f:
+                cached_uuid = f.read()
+                # Check if it's changed out from under us -- problem!
+                if existing_uuid and cached_uuid != existing_uuid:
+                    log.error('host_uuid changed on disk unexpectedly!'
+                              '\nPrevious: {0}\nNew: {1}\nKeeping previous.'
+                              .format(existing_uuid, cached_uuid))
+                    return {'host_uuid': existing_uuid}
+                return {'host_uuid': cached_uuid}
+        elif existing_uuid:
+            log.error('host_uuid was previously generated, but the cached '
+                      'file is no longer present: {0}'.format(cached_uuid_path))
+    except Exception:
+        log.exception('Problem retrieving cached host uuid from file: {0}'
+                      .format(cached_uuid_path))
+
+    # Generate a fresh one if needed
+    if not existing_uuid:
+        existing_uuid = str(uuid.uuid4())
+
+    # Cache the new (or old if it needs re-caching) uuid
+    try:
+        with open(cached_uuid_path, 'w') as f:
+            f.write(existing_uuid)
+    except Exception:
+        log.exception('Problem writing cached host uuid to file: {0}'
+                      .format(cached_uuid_path))
+
+    return {'host_uuid': existing_uuid}

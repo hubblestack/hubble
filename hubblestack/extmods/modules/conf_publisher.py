b95641f65343f805e8d41bdf60ea79fb3a119a82
Abhishek <khattri@adobe.com>
2018-03-15 12:50:10 +0530

    module to push hubble config to splunk

---
+++ hubblestack/extmods/modules/conf_publisher.py
@@ -0,0 +1,53 @@
+
+import logging
+import hubblestack.splunklogging
+import copy
+import time
+
+log = logging.getLogger(__name__)
+
+def publish():
+    log.info('Started publishing config to splunk')
+    
+    initialize_splunklogging()
+    filtered_conf=filter_config()
+    class MockRecord(object):
+            def __init__(self, message, levelname, asctime, name):
+                self.message = message
+                self.levelname = levelname
+                self.asctime = asctime
+                self.name = name
+
+    handler = hubblestack.splunklogging.SplunkHandler()
+    handler.emit(MockRecord(filtered_conf, 'INFO', time.asctime(), 'hubblestack.hubble_config'))
+    log.info('Published config to splunk')	
+
+def initialize_splunklogging():
+    hubblestack.splunklogging.__grains__ = __grains__
+    hubblestack.splunklogging.__salt__ = __salt__
+
+#Filter out keys containing certain patterns to avoid sensitive information being sent to splunk
+def filter_config():
+    opts_copy = copy.deepcopy(__opts__)
+	
+    if (__salt__['config.get']('hubblestack:conf_publisher:patterns_to_skip')):
+        str_patterns_to_filter = __salt__['config.get']('hubblestack:conf_publisher:patterns_to_skip')
+        patterns_to_filter=str_patterns_to_filter.split(",")
+    else:
+        patterns_to_filter = ["password", "token"]
+    
+    filtered_conf = remove_sensitive_info(opts_copy, patterns_to_filter)
+
+    return filtered_conf
+
+#Recursively removes key value pairs where key contains any of patterns_to_filter
+def remove_sensitive_info(obj, patterns_to_filter):
+    if isinstance(obj, dict):
+         obj = {
+             key: remove_sensitive_info(value, patterns_to_filter)
+             for key, value in obj.iteritems()
+             if not any(patt in key for patt in patterns_to_filter)}
+    elif isinstance(obj, list):
+         obj = [remove_sensitive_info(item, patterns_to_filter)
+                    for item in obj]
+    return obj

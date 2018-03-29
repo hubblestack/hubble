a6f5638a62128d9215b28e26ed5cd3876b55b324
Paul Miller <paul@jettero.pl>
2018-03-29 14:39:34 -0400

    remove my contrib dir of goofy tricks

--- contrib/paul/profile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-hubble="$(which hubble)"
-profile="${1:-/tmp/profile}"
-
-# python -m cProfile -o "$profile" "$hubble"
-python -c "import pstats; p = pstats.Stats('$profile'); p.strip_dirs().sort_stats(-1).print_stats()"
--- contrib/paul/pulsar-pytest.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-export DEBUG_SHOW_PULSAR_LOGS=1
-export PYTHONPATH=.
-
-pytest --tb=short -xv tests/unittests/test_pulsar.py

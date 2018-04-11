1ab322d8718df345383c33b9843edda1ab00bdd4
Colton Myers <colton.myers@gmail.com>
2018-04-11 10:23:51 -0600

    Fix for NameError if the config isn't defined
    Also did a little linting and added some more docs

--- hubblestack/extmods/grains/custom_grains_pillar.py
+++ hubblestack/extmods/grains/custom_grains_pillar.py
@@ -1,6 +1,9 @@
 '''
 HubbleStack Custom Grains and Pillar
 
+Allows for fetching custom grain and pillar data from a local salt-minion via
+salt-call
+
 :maintainer: HubbleStack
 :platform: All
 :requires: SaltStack
@@ -20,27 +23,31 @@ __salt__ = {
 
 def populate_custom_grains_and_pillar():
     '''
-    To return grains and pillar fields values as specified in config file.
+    Populate local salt-minion grains and pillar fields values as specified in
+    config file.
+
     For example:
-    custom_grains_pillar:
-      grains:
-        - selinux: selinux:enabled
-        - release: osrelease
-      pillar:
-        - ntpserver: network_services:ntpserver
+
+        custom_grains_pillar:
+          grains:
+            - selinux: selinux:enabled
+            - release: osrelease
+          pillar:
+            - ntpserver: network_services:ntpserver
+
+    Note that the core grains are already included in hubble grains -- this
+    is only necessary for custom grains and pillar data.
     '''
     log.debug('Fetching custom grains and pillar details')
     grains = {}
     salt.modules.config.__opts__ = __opts__
-    if __salt__['config.get']('custom_grains_pillar:grains'):
-        custom_grains = __salt__['config.get']('custom_grains_pillar:grains')
+    custom_grains = __salt__['config.get']('custom_grains_pillar:grains', [])
     for grain in custom_grains:
         for key in grain:
             if _valid_command(grain[key]):
                 value = __salt__['cmd.run']('salt-call grains.get {0}'.format(grain[key])).split('\n')[1].strip()
                 grains[key] = value
-    if __salt__['config.get']('custom_grains_pillar:pillar'):
-        custom_pillar = __salt__['config.get']('custom_grains_pillar:pillar')
+    custom_pillar = __salt__['config.get']('custom_grains_pillar:pillar', [])
     for pillar in custom_pillar:
         for key in pillar:
             if _valid_command(pillar[key]):
@@ -51,6 +58,9 @@ def populate_custom_grains_and_pillar():
 
 
 def _valid_command(string):
+    '''
+    Check for invalid characters in the pillar or grains key
+    '''
     invalid_characters = re.findall('[^a-zA-Z0-9:_-]',string)
     if len(invalid_characters) > 0:
         log.info("Command: {0} contains invalid characters: {1}".format(string, invalid_characters))

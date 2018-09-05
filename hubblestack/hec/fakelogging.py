03b5d83f2e7473055ac4be66d172ccae6a88c23f
Paul Miller <paul@jettero.pl>
2018-09-05 11:14:15 -0400

    finalize disk queueing and some fake logging (to prevent log→ hec→log→hec loops)

---
+++ hubblestack/hec/fakelogging.py
@@ -0,0 +1,28 @@
+
+import os
+import time
+
+class FakeLog(object):
+    ''' teach the hec objects about logging
+        in such a way that it can never accidentally go to slunk
+        (causing an infinite loop of logging)
+    '''
+    def __init__(self, name):
+        self.file = os.environ.get('HUBBLE_HEC_DEBUG')
+        self.name = 'hubblestack.hec.' + name
+    def _show(self, level, fmt, *a):
+        if not self.file:
+            return
+        p = os.getpid()
+        t = time.ctime()
+        with open(self.file, 'a') as fh:
+            msg = fmt % a
+            for line in msg.splitlines():
+                fh.write('{time} {name} [{pid}] {}: {}\n'.format(level, line,
+                    time=t, pid=p, name=self.name))
+    def debug(self, *a): self._show('debug', *a)
+    def  info(self, *a): self._show('info',  *a)
+    def error(self, *a): self._show('error', *a)
+
+def getLogger(name):
+    return FakeLog(name)
--- hubblestack/hec/obj.py
+++ hubblestack/hec/obj.py
@@ -1,25 +1,31 @@
 
-from collections import Counter
-
 import socket
 import json
 import time
 import copy
 import os
+import hashlib
 
 import certifi
 import urllib3
 
-import logging
-
 from . dq import DiskQueue
+
+# fakelogging goes to a file iff specified in environment
+# and otherwise does nothing (for debugging)
+import fakelogging as logging
+log = logging.getLogger('obj')
 
 __version__ = '1.0'
 
-log = logging.getLogger('hubblestack.hec')
-
 _max_content_bytes = 100000
 http_event_collector_debug = False
+
+# the list of collector URLs given to the HEC object
+# are hashed into an md5 string that identifies the URL set
+# these maximums are per URL set, not for the entire disk cache
+max_diskqueue_items = 200
+max_diskqueue_size  = 10 * (1024 ** 2)
 
 class Payload(object):
     ''' formatters for final payload stringification
@@ -50,16 +56,17 @@ class Payload(object):
         return cls(payload)
 
     @classmethod
-    def promote(cls, payload, eventtime=''):
+    def promote(cls, payload, eventtime='', no_queue=False):
         if isinstance(payload, cls):
             return payload
-        return Payload(payload, eventtime=eventtime)
+        return Payload(payload, eventtime=eventtime, no_queue=no_queue)
 
-    def __init__(self, dat, eventtime=''):
+    def __init__(self, dat, eventtime='', no_queue=False):
         if self.host is None:
             self.__class__.host = socket.gethostname()
 
-        self.retries = Counter()
+        self.requeues = 0
+        self.no_queue = no_queue or dat.pop('_no_queue', False)
 
         if 'host' not in dat or dat['host'] is None:
             dat['host'] = self.host
@@ -107,14 +114,34 @@ class Payload(object):
 # occur if next event payload will exceed limit
 
 class HEC(object):
+    flushing_queue = False
+
+    class Server(object):
+        bad = False
+        def __init__(self, host, port=8080, proto='https'):
+            if '://' in host:
+                proto,host = host.split('://')
+            if ':' in host:
+                host,port = host.split(':')
+            self.uri = '{proto}://{host}:{port}/services/collector/event'.format(
+                proto=proto, host=host, port=port)
+            self.fails = 0
+
+        def __str__(self):
+            r = self.uri
+            if self.fails:
+                r += ' (fails: {0})'.format(self.fails)
+            return r
+
+
     def __init__(self, token, http_event_server, host='', http_event_port='8088',
                  http_event_server_ssl=True, http_event_collector_ssl_verify=True,
-                 max_bytes=_max_content_bytes, proxy=None, timeout=9.05):
+                 max_bytes=_max_content_bytes, proxy=None, timeout=9.05,
+                 disk_queue='/var/cache/hubble/dq'):
 
-        self.payload_retry_before_fail =  5
+        self.max_requeues =  5
         self.queue_overflow_msg_delay = 10
         self.retry_diskqueue_interval = 60
-        self._last_dq_flush_attempt = 0
 
         self.timeout = timeout
         self.token = token
@@ -147,13 +174,9 @@ class HEC(object):
             servers = [servers]
         for server in servers:
             if http_event_server_ssl:
-                self.server_uri.append(['https://%s:%s/services/collector/event' % (server, http_event_port), True])
+                self.server_uri.append(self.Server(server, http_event_port, proto='https'))
             else:
-                self.server_uri.append(['http://%s:%s/services/collector/event' % (server, http_event_port), True])
-
-        if http_event_collector_debug:
-            print self.token
-            print self.server_uri
+                self.server_uri.append(self.Server(server, http_event_port, proto='http'))
 
         # build headers once
         self.headers = urllib3.make_headers( keep_alive=True,
@@ -169,7 +192,7 @@ class HEC(object):
         pm_kw = {
             'timeout': self.timeout,
             'retries': urllib3.util.retry.Retry(
-                total=5, redirect=10, backoff_factor=3,
+                total=3, redirect=10, backoff_factor=3,
                 connect=self.timeout, read=self.timeout,
                 respect_retry_after_header=True)
         }
@@ -184,71 +207,106 @@ class HEC(object):
         else:
             self.pool_manager = urllib3.PoolManager(**pm_kw)
 
-        self.queue = DiskQueue()
+        md5 = hashlib.md5()
+        uril = sorted([ x.uri for x in self.server_uri ])
+        for u in uril:
+            md5.update(u)
+        actual_disk_queue = os.path.join(disk_queue, md5.hexdigest())
+        log.debug("disk_queue for %s: %s", uril, actual_disk_queue)
+        self.queue = DiskQueue(actual_disk_queue, restrict_to=Payload,
+            max_items=max_diskqueue_items, max_size=max_diskqueue_size)
+
 
     def queueEvent(self, dat, eventtime=''):
         if not isinstance(dat, Payload):
-            payload = Payload(dat, eventtime)
+            payload = Payload(dat, eventtime, no_queue=no_queue)
+        if payload.no_queue: # here you silly hec, queue this no_queue payload...
+            return
         self.queue.put(payload)
 
+
     def flushQueue(self):
-        for i in self.queue:
-            self.batchEvent(i)
+        if self.flushing_queue:
+            log.debug('already flushing queue')
+            return
+        if self.queue.eventcount < 1:
+            log.debug('nothing in queue')
+            return
+        dt = time.time() - self.queue.last_get
+        if dt >= self.retry_diskqueue_interval and self.queue.eventcount:
+            log.debug('flushing queue eventscount=%d', self.queue.eventcount)
+        self.flushing_queue = True
+        while self.flushing_queue:
+            x = self.queue.pull()
+            if not x:
+                break
+            self.batchEvent(x)
         self.flushBatch()
+        self.flushing_queue = False
 
-    def _attempt_flush_queue(self):
-        t = time.time()
-        dt = t - self._last_dq_flush_attempt
-        self._last_dq_flush_attempt = time.time()
-        if dt >= self.retry_diskqueue_interval and self.queue.size:
-            log.debug('flushing queue sz=%d', self.queue.size)
-            self.flushQueue()
 
-    def _requeue(self, payloads, server):
-        server_maybe_bad = False
+    def _requeue(self, payloads):
+        log.debug("_requeue")
+        if self.flushing_queue:
+            log.debug("aborting queue flush due to requeue")
+            self.flushing_queue = False
+        if not isinstance(payloads, (list,tuple)):
+            payloads = [ payloads ]
         for pload in payloads:
-            pload.retries[ server ] += 1
-            if pload.retries[ server ] < self.payload_retry_before_fail:
+            if pload.no_queue:
+                continue
+            if pload.requeues < self.max_requeues:
+                log.debug("requeueing payload (requeues so far: %d)", pload.requeues)
+                pload.requeues += 1
                 self.queue.put(pload)
-            else:
-                server_maybe_bad = True
-        return server_maybe_bad
+
 
     def _send(self, *payload):
         data = ' '.join([ str(x) for x in payload ])
-        self.server_uri = [x for x in self.server_uri if x[1] is not False]
-        for server in self.server_uri:
+
+        servers = [ x for x in self.server_uri if not x.bad ]
+        if not servers:
+            log.error("all servers are marked 'bad', aborting send")
+            return
+
+        # This logic is overly complicated originally the plan was to have the
+        # HEC() object manage multiple servers and choose the successful one to
+        # send to. Mostly if the hubble configs contain multiple endpoints,
+        # hubble sends to all of them, not just the first successful one.  and
+        # mostly that logic is handled outside of the HEC class (see
+        # splunklogging.py)
+        for server in sorted(servers, key=lambda u: u.fails):
             try:
-                r = self.pool_manager.request('POST', server[0], body=data, headers=self.headers)
+                r = self.pool_manager.request('POST', server.uri, body=data, headers=self.headers)
+                server.fails = 0
             except urllib3.exceptions.LocationParseError as e:
                 if self.log_other_exceptions:
-                    log.error('server uri parse error "{0}": {1}'.format(server[0], e))
-                server[1] = False
+                    log.error('server uri parse error "{0}": {1}'.format(server.uri, e))
+                server.bad = True
                 continue
             except Exception as e:
-                server[1] = False
+                server.fails += 1
                 continue
-            if r.status >= 400:
-                server_maybe_bad = self._requeue(payload, server[0])
-                if server_maybe_bad:
-                    server[1] = False
-                #self._requeue(self, payload, 'blah')
-            else:
+            if r.status < 400:
                 return r
 
-    def sendEvent(self, payload, eventtime=''):
-        self._attempt_flush_queue()
-        r = self._send( Payload.promote(payload, eventtime=eventtime) )
-
-        if http_event_collector_debug:
-            if r is not None and hasattr(r, 'text'):
-                log.debug(r.text)
-            log.debug(payload)
+        log.error('failed to send payload, queueing for later delivery')
+        self._requeue(payload)
 
 
-    def batchEvent(self, dat, eventtime=''):
-        self._attempt_flush_queue()
-        payload = Payload.promote(dat, eventtime)
+    def _finish_send(self, r):
+        if r is not None and hasattr(r, 'text'):
+            log.debug(r.text)
+            self.flushQueue()
+
+
+    def sendEvent(self, payload, eventtime='', no_queue=False):
+        r = self._send( Payload.promote(payload, eventtime=eventtime, no_queue=no_queue) )
+        self._finish_send(r)
+
+
+    def batchEvent(self, dat, eventtime='', no_queue=False):
+        payload = Payload.promote(dat, eventtime, no_queue=False)
 
         if (self.currentByteLength + len(payload)) > self.maxByteLength:
             self.flushBatch()
@@ -264,5 +322,6 @@ class HEC(object):
             r = self._send( *self.batchEvents )
             self.batchEvents = []
             self.currentByteLength = 0
+            self._finish_send(r)
 
 http_event_collector = HEC

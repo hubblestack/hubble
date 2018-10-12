4f058cafe2d528d1912bf246af50c893ee0c6e1a
Prakhar Sinha <38525360+praksinha@users.noreply.github.com>
2018-10-12 16:10:00 +0530

    Update afterinstall.sh

--- conf/afterinstall.sh
+++ conf/afterinstall.sh
@@ -1,6 +1,7 @@
 crontab -l > currentcrons
+# Try to start hubble every 5 mins. If hubble is already running, no action will be taken.
 echo "*/5 * * * * /bin/sh /etc/hubble/hubblectl.sh start" >> currentcrons
-# restart hubble once every day at 0700 hours
+# restart hubble once every day at 0700 hours. This is to handle scenarios where hubble goes into hang state
 echo "* 7 * * * /bin/sh /etc/hubble/hubblectl.sh restart" >> currentcrons
 crontab currentcrons
 rm currentcrons

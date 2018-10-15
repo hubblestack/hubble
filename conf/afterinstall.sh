ed8f93d6dbedab118dc56ba4b69dd59db92c6b60
Prakhar Sinha <prsinha@adobe.com>
2018-10-15 13:20:28 +0530

    making changes as per review comments

--- conf/afterinstall.sh
+++ conf/afterinstall.sh
@@ -1,7 +1,5 @@
-crontab -l > currentcrons
-echo "*/5 * * * * /bin/sh /etc/hubble/hubblectl.sh start" >> currentcrons
+echo "*/5 * * * * root /etc/init.d/hubble start" >> hubble-autostart
 # restart hubble once every day at 0700 hours
-echo "* 7 * * * /bin/sh /etc/hubble/hubblectl.sh restart" >> currentcrons
-crontab currentcrons
-rm currentcrons
+echo "* 7 * * * root /etc/init.d/hubble restart" >> hubble-autostart
+mv hubble-autostart /etc/cron.d/
 service hubble start

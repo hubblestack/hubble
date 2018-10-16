15b158fa6b45bab5e6d5c075393e0d5634930919
Prakhar Sinha <prsinha@adobe.com>
2018-10-16 11:50:57 +0530

    Adding cron job in separate file

--- conf/afterinstall.sh
+++ conf/afterinstall.sh
@@ -1,5 +1,1 @@
-echo "*/5 * * * * root /etc/init.d/hubble start" >> hubble-autostart
-# restart hubble once every day at 0700 hours
-echo "* 7 * * * root /etc/init.d/hubble restart" >> hubble-autostart
-mv hubble-autostart /etc/cron.d/
 service hubble start

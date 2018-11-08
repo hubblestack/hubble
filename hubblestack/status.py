0141e81d3ad683fa57af53268f31510e59cfe066
Paul Miller <paul@jettero.pl>
2018-11-08 10:01:22 -0500

    add durations as optional stats; add set_status_dumpster() location

--- hubblestack/status.py
+++ hubblestack/status.py
@@ -2,6 +2,10 @@ import time
 import json
 from collections import namedtuple
 
+import signal
+
+DUMPSTER = '/var/cache/hubble/status.json'
+
 class HubbleStatusResourceNotFound(Exception):
     pass
 
@@ -12,7 +16,8 @@ class HubbleStatus(object):
             self.last_t = self.start = time.time()
             self.count  = 0
             self.ema_dt = None
-            self.ema_var = None
+            self.dur = None
+            self.ema_dur = None
 
         @property
         def dt(self):
@@ -20,8 +25,10 @@ class HubbleStatus(object):
 
         @property
         def asdict(self):
-            return { 'start': self.start, 'count': self.count, 'dt': self.dt,
-                'ema_dt': self.ema_dt, 'ema_var': self.ema_var }
+            r = { 'start': self.start, 'count': self.count, 'dt': self.dt, 'ema_dt': self.ema_dt }
+            if self.dur is not None:
+                r.update({'dur': self.dur, 'ema_dur': self.ema_dur})
+            return r
 
         def mark(self):
             t = time.time()
@@ -29,8 +36,15 @@ class HubbleStatus(object):
             dt = self.dt
             self.last_t = t
             self.ema_dt  = dt if self.ema_dt is None else 0.5*self.ema_dt + 0.5*dt
-            var = (self.ema_dt - dt) ** 2
-            self.ema_var = var if self.ema_var is None else 0.5*self.ema_var + 0.5*var
+
+        def fin(self):
+            self.dur = self.dt
+            self.ema_dur  = self.dur if self.ema_dur is None else 0.5*self.ema_dur + 0.5*self.dur
+
+    @classmethod
+    def set_status_dumpster(cls, loc):
+        global DUMPSTER
+        DUMPSTER = loc
 
     def __init__(self, namespace, *resources):
         if namespace is None:
@@ -62,6 +76,10 @@ class HubbleStatus(object):
     def mark(self, n):
         n = self._checkmark(n)
         self.dat[n].mark()
+
+    def fin(self, n):
+        n = self._checkmark(n)
+        self.dat[n].fin()
 
     @property
     def asdict(self):

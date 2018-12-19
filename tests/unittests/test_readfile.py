eba58a59c3d5c1f005e5444ba2d1ec3c3c962b8b
Emilia Voicu <emilia.voicu95@gmail.com>
2018-12-19 07:51:58 -0800

    add tests for readfile.yaml

--- tests/unittests/test_readfile.py
+++ tests/unittests/test_readfile.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import json
 import os
 import sys
+import yaml
 import pytest
 
 myPath = os.path.abspath(os.getcwd())
@@ -12,16 +13,24 @@ import hubblestack.extmods.fdg.readfile
 
 class TestReadfile():
 
+    def generate_data(self):
+        '''
+        Helping function to generate dict data to populate json/yaml files
+        '''
+        sample_data = {"id": "file",
+                       "value": {"key1": "value1",
+                                 "key2": {"key3": "value2"}},
+                       "menuitem": ["item1", "item2", "item3"]}
+        return sample_data
+ 
     @pytest.fixture(scope="session")
     def json_file(self, tmpdir_factory):
         '''
         Helping function that creates a ``.json`` sample file to test against
         '''
-        sample_json = {"id": "file",
-                       "value": {"key1": "value1",
-                                 "key2": {"key3": "value2"}},
-                       "menuitem": ["item1", "item2", "item3"]}
+        sample_json = self.generate_data() 
         json_file = tmpdir_factory.mktemp("data").join("json_file.json")
+
         json_file.write(str(json.dumps(sample_json)))
 
         return str(json_file)
@@ -116,5 +125,110 @@ class TestReadfile():
             invalid_file.write("invalijson")
         expected_status, expected_ret = False, None
         status, ret = hubblestack.extmods.fdg.readfile.json(json_file, subkey='id')
+        assert expected_status == status
+        assert expected_ret == ret
+
+
+    @pytest.fixture(scope="session")
+    def yaml_file(self, tmpdir_factory):
+        '''
+        Helping function that creates a ``.yaml`` sample file to test against
+        '''
+        sample_yaml= self.generate_data()
+        yaml_file = tmpdir_factory.mktemp("data").join("yaml_file.yaml")
+        yaml_file.write(str(yaml.dump(sample_yaml)))
+
+        return str(yaml_file)
+
+    def test_yaml_InvalidPath_EmptyReturn(self):
+        '''
+        Test that given an invalid path, the yaml function returns False status
+        and an empty return value
+        '''
+        expected_status, expected_ret = False, None
+        status, ret = hubblestack.extmods.fdg.readfile.yaml('/invalid/path')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_SingleSubkey_ReturnsValue(self, yaml_file):
+        '''
+        Test that given a single subkey argument, the function extracts the appropriated value
+        '''
+        expected_status, expected_ret = True, "file"
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(yaml_file, subkey='id')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_InvalidSingleSubkey_EmptyReturn(self, yaml_file):
+        '''
+        Test that given an invalid single subkey argument,
+        the function returns False status and empty value
+        '''
+        expected_status, expected_ret = False, None
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(yaml_file, subkey='invalid_key')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_MultipleSubkeys_ReturnsValue(self, yaml_file):
+        '''
+        Test that given multiple subkeys, separated by a valid separator,
+        the function returns the appropriate value
+        '''
+        expected_status, expected_ret = True, "value2"
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(
+                yaml_file, subkey='value,key2,key3', sep=',')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_InvalidSep_EmptyReturn(self, yaml_file):
+        '''
+        Test that given multiple subkeys separated by an invalid ``sep``,
+        the function returns a False status and None value
+        '''
+        expected_status, expected_ret = False, None
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(
+                yaml_file, subkey='value,key2,key3', sep='/')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_IndexSubkey_ReturnsValue(self, yaml_file):
+        '''
+        Test that given an index as subkey, the function returns the appropriate value
+        '''
+        expected_status, expected_ret = True, "item2"
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(
+                yaml_file, subkey='menuitem,1', sep=',')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_InvalidIndexSubkey_EmptyReturn(self, yaml_file):
+        '''
+        Test that given an index as subkey that exceeds the list length,
+        the function returns False status and None value
+        '''
+        expected_status, expected_ret = False, None
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(yaml_file, subkey='menuitem,15', sep=',')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_EmptyFile_EmptyReturn(self, yaml_file):
+        '''
+        Test that given an empty yaml file, the function returns False status and None value
+        '''
+        with open(yaml_file, 'r+') as invalid_file:
+            invalid_file.truncate(0)
+        expected_status, expected_ret = False, None
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(yaml_file, subkey='id')
+        assert expected_status == status
+        assert expected_ret == ret
+
+    def test_yaml_InvalidJsonFile_EmptyReturn(self, yaml_file):
+        '''
+        Test that given an invalid yaml file, the function returns False status and None value
+        '''
+        with open(yaml_file, 'w+') as invalid_file:
+            invalid_file.write("invalidyaml")
+        expected_status, expected_ret = False, None
+        status, ret = hubblestack.extmods.fdg.readfile.yaml(yaml_file, subkey='id')
         assert expected_status == status
         assert expected_ret == ret

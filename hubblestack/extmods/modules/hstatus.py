9733b1c6bcfab7b0080c758b5f54b653cf0ecac3
Paul Miller <paul@jettero.pl>
2018-12-30 11:41:41 -0500

    this is just about right, except the missing reset

--- hubblestack/extmods/modules/hstatus.py
+++ hubblestack/extmods/modules/hstatus.py
@@ -1,5 +1,9 @@
 
+import re
+import logging
 import hubblestack.status
+
+log = logging.getLogger(__name__)
 
 __virtualname__ = 'hstatus'
 
@@ -11,7 +15,39 @@ def get():
 
         (probably only useful from other excution modules)
     '''
-    return hubblestack.status.HubbleStatus.as_json()
+    return hubblestack.status.HubbleStatus.stats()
+
+def msg_counts(pat=r'hubblestack.hec.obj.input:(?P<stype>[^:]+)', reset=True):
+    ''' returns counter data formatted for the splunk_generic_return returner
+
+        params:
+            pat - the key matching algorithm is a simple regular expression
+    '''
+
+    if reset:
+        log.error("TODO: reset timers")
+
+    km = re.compile(pat)
+    r = list()
+    s = get()
+    for k,v in s.iteritems():
+        try:
+            if v['first_t'] == 0 or v['last_t'] == 0:
+                continue
+        except KeyError:
+            continue
+        m = km.match(k)
+        if m:
+            d = m.groupdict()
+            d.update({ 'count': v['count'], 'start': v['first_t'], 'end': v['last_t'] })
+            r.append(d)
+    if r:
+        min_time = min([ x['start'] for x in r ])
+        return {
+            'sourcetype': 'hubble_hec_counters',
+            'time': str(int(min_time)),
+            'events': r
+        }
 
 def dump():
     ''' trigger a dump to the status.json file as described in hubblestack.status
@@ -19,4 +55,4 @@ def dump():
         This is intended to be invoked from a daemon schedule and is probably
         not useful outside that context.
     '''
-    return hubblestack.status.HubbleStatus.dumpster_fire()
+    hubblestack.status.HubbleStatus.dumpster_fire()

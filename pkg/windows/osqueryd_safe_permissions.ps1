0d714881ab93e7a24a6462c437714a0f5104d3c8
Mudit Agarwal <muditagr90@gmail.com>
2019-01-03 15:09:16 +0530

    Create osquerd_safe_permission check

---
+++ pkg/windows/osqueryd_safe_permissions.ps1
@@ -0,0 +1,36 @@
+    $osqueryd_path = "$INSTDIR\osqueryd\"
+    $acl = Get-Item $osqueryd_path |get-acl
+    $acl.SetAccessRuleProtection($true,$true)
+    $acl |Set-Acl
+
+    $group = "NT SERVICE\TrustedInstaller"
+    $acl = Get-Acl $osqueryd_path
+    $inherit =[system.security.accesscontrol.InheritanceFlags]"ContainerInherit,ObjectInherit"
+    $propagation =[system.security.accesscontrol.PropagationFlags]"None"
+    $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($group,"FullControl", $inherit, $Propagation ,,,"Allow")
+    $acl.RemoveAccessRuleAll($accessrule)
+    set-acl -aclobject $acl $osqueryd_path
+
+    $group = "ALL APPLICATION PACKAGES"
+    $acl = Get-Acl $osqueryd_path
+    $inherit =[system.security.accesscontrol.InheritanceFlags]"ContainerInherit,ObjectInherit"
+    $propagation =[system.security.accesscontrol.PropagationFlags]"None"
+    $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($group,"FullControl", $inherit, $Propagation ,,,"Allow")
+    $acl.RemoveAccessRuleAll($accessrule)
+    set-acl -aclobject $acl $osqueryd_path
+
+    $group = "ALL RESTRICTED APPLICATION PACKAGES"
+    $acl = Get-Acl $osqueryd_path
+    $inherit =[system.security.accesscontrol.InheritanceFlags]"ContainerInherit,ObjectInherit"
+    $propagation =[system.security.accesscontrol.PropagationFlags]"None"
+    $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($group,"FullControl", $inherit, $Propagation ,,,"Allow")
+    $acl.RemoveAccessRuleAll($accessrule)
+    set-acl -aclobject $acl $osqueryd_path
+
+    $group = "CREATOR OWNER"
+    $acl = Get-Acl $osqueryd_path
+    $inherit =[system.security.accesscontrol.InheritanceFlags]"ContainerInherit,ObjectInherit"
+    $propagation =[system.security.accesscontrol.PropagationFlags]"None"
+    $accessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($group,"FullControl", $inherit, $Propagation ,,,"Allow")
+    $acl.RemoveAccessRuleAll($accessrule)
+    set-acl -aclobject $acl $osqueryd_path

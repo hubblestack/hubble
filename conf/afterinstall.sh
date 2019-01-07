0ad644f79fed40e6fe089c4c4bbcf65308fd7656
Prakhar Sinha <prsinha@adobe.com>
2019-01-07 12:17:36 +0530

    rebasing

--- conf/afterinstall.sh
+++ conf/afterinstall.sh
@@ -1,8 +1,5 @@
-crontab -l > currentcrons
-# Try to start hubble every 5 mins. If hubble is already running, no action will be taken.
-echo "*/5 * * * * /bin/sh /etc/hubble/hubblectl.sh start" >> currentcrons
-# restart hubble once every day at 0700 hours. This is to handle scenarios where hubble goes into hang state
-echo "* 7 * * * /bin/sh /etc/hubble/hubblectl.sh restart" >> currentcrons
-crontab currentcrons
-rm currentcrons
+echo "*/5 * * * * root /etc/init.d/hubble start" >> hubble-autostart
+# restart hubble once every day at 0700 hours
+echo "* 7 * * * root /etc/init.d/hubble restart" >> hubble-autostart
+mv hubble-autostart /etc/cron.d/
 service hubble start

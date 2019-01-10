74d989cd814a9634532b0fc46b44e0c25bfa75b9
Paul Miller <paul@jettero.pl>
2019-01-10 09:27:47 -0500

    remove the hstatus.get, remove fudging msg_counts for self-type, fix minor bugs

--- hubblestack/extmods/modules/hstatus.py
+++ hubblestack/extmods/modules/hstatus.py
@@ -9,31 +9,26 @@ log = logging.getLogger(__name__)
 __virtualname__ = 'hstatus'
 
 SOURCETYPE = 'hubble_audit_summary'
+MSG_COUNTS_PAT = r'hubblestack.hec.obj.input:(?P<stype>[^:]+)'
 
 def __virtual__():
     return True
 
-def get():
-    ''' return the counters and timing status tracked by hubblestack.status
-
-        (probably only useful from other excution modules)
-    '''
-    return hubblestack.status.HubbleStatus.stats()
-
-def msg_counts(pat=r'hubblestack.hec.obj.input:(?P<stype>[^:]+)', reset=True):
+def msg_counts(pat=MSG_COUNTS_PAT, reset=True, emit_self=False, sourcetype=SOURCETYPE):
     ''' returns counter data formatted for the splunk_generic_return returner
 
         params:
-            pat   - the key matching algorithm is a simple regular expression
-            reset - whether or not to reset the counters returned
+            pat        - the key matching algorithm is a simple regular expression
+                         (default: hstatus.MSG_COUNTS_PAT)
+            reset      - whether or not to reset the counters returned (default: True)
+            emit_self  - whether to emit sourcetype counters (default: False)
+            sourcetype - the sourcetype for the accounting messages (default: hstatus.SOURCETYPE)
     '''
 
     pat = re.compile(pat)
     ret = list() # events to return
-    got_stats = get()
-    fudge_me = None
     to_reset = set()
-    for k,v in got_stats.iteritems():
+    for k,v in hubblestack.status.HubbleStatus.stats().iteritems():
         try:
             if v['first_t'] == 0 or v['last_t'] == 0:
                 continue
@@ -41,28 +36,22 @@ def msg_counts(pat=r'hubblestack.hec.obj.input:(?P<stype>[^:]+)', reset=True):
             continue
         m = pat.match(k)
         if m:
-            # first, populate d with {'stype': 'sourcetypehere'}
-            d = m.groupdict()
-            # then add the stats
-            d.update({ 'event_count': v['count'], 'send_session_start': int(v['first_t']),
-                'send_session_end': int(math.ceil(v['last_t'])) })
-            ret.append(d)
-            # keep a pointer to the hec_counters
-            # they'll need to be fixed later
-            if d['stype'] == SOURCETYPE:
-                fudge_me = d
-        to_reset.add(k)
+            try:
+                stype = m.groupdict()['stype']
+            except KeyError:
+                continue
+            if emit_self or stype != sourcetype:
+                ret.append({ 'stype': stype,
+                    'event_count': v['count'],
+                    'send_session_start': int(v['first_t']),
+                    'send_session_end': int(math.ceil(v['last_t'])) })
+            to_reset.add(k)
 
     if ret:
         if reset:
             for k in to_reset:
                 hubblestack.status.HubbleStatus.reset(k)
-        min_time = min([ x['send_session_start'] for x in ret ])
-        if fudge_me:
-            # this is a fudge factor for the recursion where we report on own
-            # sourcetype missing the currently-being-submitted counts
-            fudge_me['event_count'] += len(ret)
-        return { 'sourcetype': SOURCETYPE, 'time': min_time, 'events': ret }
+        return { 'sourcetype': sourcetype, 'events': ret }
 
 def dump():
     ''' trigger a dump to the status.json file as described in hubblestack.status
@@ -70,4 +59,4 @@ def dump():
         This is intended to be invoked from a daemon schedule and is probably
         not useful outside that context.
     '''
-    hubblestack.status.HubbleStatus.dumpster_fire()
+    return hubblestack.status.HubbleStatus.dumpster_fire()

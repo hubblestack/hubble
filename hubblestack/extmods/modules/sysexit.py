6c869f9cccde045797773afff607750f4c753407
Paul Miller <paul@jettero.pl>
2019-01-29 08:25:10 -0500

    sys.exit … this probably isn't a good idea... thinking

---
+++ hubblestack/extmods/modules/sysexit.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+''' intended for testing, this module's sole purpose is to cause the running
+daemon to exit gracefully within a scheduled timeframe '''
+
+from __future__ import absolute_import
+
+import logging
+import sys
+
+from salt.exceptions import CommandExecutionError
+
+log = logging.getLogger(__name__)
+
+def sysexit(command):
+    log.info('instructing daemon to exit')
+    sys.exit(0)

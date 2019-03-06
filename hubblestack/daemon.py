c7a3618ad417f16d6737ea3c105b9c6493cd4bff
Chandler Newby <chandler.newby@gmail.com>
2019-03-06 15:15:05 -0700

    Attempt to detect cloned hubble installs

--- hubblestack/daemon.py
+++ hubblestack/daemon.py
@@ -26,6 +26,7 @@ import salt.utils
 import salt.utils.platform
 import salt.utils.jid
 import salt.utils.gitfs
+import salt.utils.path
 import salt.log.setup
 import hubblestack.splunklogging
 import hubblestack.hec.opt
@@ -646,6 +647,33 @@ def load_config():
     os.chmod(__opts__['log_file'], 384)
     os.chmod(parsed_args.get('configfile'), 384)
 
+    # Check for a cloned system with existing hubble_uuid
+    cached_uuid_path = os.path.join(os.path.dirname(__opts__['configfile']), 'hubble_cached_uuid')
+    cached_system_uuid_path = os.path.join(os.path.dirname(__opts__['configfile']),
+                                           'hubble_cached_system_uuid')
+
+    try:
+        if os.path.isfile(cached_uuid_path) and os.path.isfile(cached_system_uuid_path):
+            with open(cached_uuid_path, 'r') as f, open(cached_system_uuid_path, 'r') as g:
+                cached_uuid = f.read()
+                cached_system_uuid = g.read()
+            if cached_uuid != cached_system_uuid:
+                query = '"SELECT uuid AS system_uuid FROM osquery_info;" --header=false --csv'
+                # Prefer our /opt/osquery/osqueryi if present
+                osqueryipaths = ('/opt/osquery/osqueryi', 'osqueryi', '/usr/bin/osqueryi')
+                for path in osqueryipaths:
+                    if salt.utils.path.which(path):
+                        live_uuid = __salt__['cmd.run']('{0} {1}'.format(path, query))
+                        live_uuid = str(live_uuid).upper()
+                        if len(live_uuid) == 36 and live_uuid != cached_system_uuid:
+                            log.error("potentially cloned system detected: System_uuid grain "
+                                      "previously saved on disk doesn't match live system value.\n"
+                                      "Resettig cached hubble_uuid value.")
+                            os.remove(cached_uuid_path)
+                        break
+    except Exception:
+        log.exception("Problem opening cache files while checking for previously cloned system")
+
     refresh_grains(initial=True)
 
     # splunk logs below warning, above info by default
@@ -722,6 +750,7 @@ def refresh_grains(initial=False):
         __opts__['fileserver_backend'] = __opts__['fallback_fileserver_backend']
 
     __opts__['hubble_uuid'] = __grains__.get('hubble_uuid', None)
+    __opts__['system_uuid'] = __grains__.get('system_uuid', None)
     __pillar__ = {}
     __opts__['grains'] = __grains__
     __opts__['pillar'] = __pillar__
--- hubblestack/extmods/grains/systemuuid.py
+++ hubblestack/extmods/grains/systemuuid.py
@@ -2,15 +2,18 @@
 '''
 Gather the system uuid via osquery
 '''
+import logging
+import os
 import salt.utils.path
 import salt.modules.cmdmod
 
 __salt__ = {'cmd.run': salt.modules.cmdmod._run_quiet}
+log = logging.getLogger(__name__)
 
 
 def get_system_uuid():
     '''
-    Gather the system uuid via osquery
+    Gather the system uuid via osquery and store it on disk.
 
     If osquery can't get a hardware-based value, it'll just randomly generate a new uuid every time.
     If that happens, fall back to the hubble_uuid.
@@ -18,24 +21,66 @@ def get_system_uuid():
     # Provides:
     #   system_uuid
 
-    options = '"SELECT uuid AS system_uuid FROM osquery_info;" --header=false --csv'
+    cached_system_uuid_path = os.path.join(os.path.dirname(__opts__['configfile']),
+                                           'hubble_cached_system_uuid')
+    previous_system_uuid = __opts__.get('system_uuid', None)
+
+    # Get the system uuid via osquery. If it changes, fall back to hubble_uuid
+    live_system_uuid = _get_uuid_from_system() or __opts__.get('hubble_uuid', None)
+
+    if not live_system_uuid:
+        # Can't reliably get system_uuid, and hubble_uuid not yet generated. Aborting.
+        return {}
+
+    try:
+        if os.path.isfile(cached_system_uuid_path):
+            with open(cached_system_uuid_path, 'r') as f:
+                cached_system_uuid = f.read()
+            # Check if it's changed out from under us -- problem!
+            if cached_system_uuid != live_system_uuid:
+                log.error("system_uuid on disk doesn't match live system value"
+                          '\nLive: {0}\nOn Disk: {1}\nRewriting cached value'
+                          .format(live_system_uuid, cached_system_uuid))
+                _write_system_uuid_to_file(cached_system_uuid_path, live_system_uuid)
+            return {'system_uuid': live_system_uuid}
+        elif previous_system_uuid:
+            log.error('system_uuid was previously cached, but the cached '
+                      'file is no longer present: {0}'.format(cached_system_uuid_path))
+        else:
+            log.warning('no cache file found, caching system_uuid. '
+                        '(probably not a problem)')
+    except Exception:
+        log.exception('Problem retrieving cached system uuid from file: {0}'
+                      .format(cached_system_uuid_path))
+
+    # Cache the system uuid if needed
+    _write_system_uuid_to_file(cached_system_uuid_path, live_system_uuid)
+    return {'system_uuid': live_system_uuid}
+
+
+def _write_system_uuid_to_file(path, uuid):
+    try:
+        with open(path, 'w') as f:
+            f.write(uuid)
+    except Exception:
+        log.exception('Problem writing cached system uuid to file: {0}'
+                      .format(path))
+
+
+def _get_uuid_from_system():
+    query = '"SELECT uuid AS system_uuid FROM osquery_info;" --header=false --csv'
 
     # Prefer our /opt/osquery/osqueryi if present
     osqueryipaths = ('/opt/osquery/osqueryi', 'osqueryi', '/usr/bin/osqueryi')
-    grains = {}
     for path in osqueryipaths:
         if salt.utils.path.which(path):
-            first_run = __salt__['cmd.run']('{0} {1}'.format(path, options))
+            first_run = __salt__['cmd.run']('{0} {1}'.format(path, query))
             first_run = str(first_run).upper()
 
-            second_run = __salt__['cmd.run']('{0} {1}'.format(path, options))
+            second_run = __salt__['cmd.run']('{0} {1}'.format(path, query))
             second_run = str(second_run).upper()
 
             if len(first_run) == 36 and first_run == second_run:
-                grains = {"system_uuid": first_run}
+                return first_run
             else:
-                existing_uuid = __opts__.get('hubble_uuid', None)
-                if existing_uuid:
-                    {"system_uuid": existing_uuid}
-            break
-    return grains
+                return None

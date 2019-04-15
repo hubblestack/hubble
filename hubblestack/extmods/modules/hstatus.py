3e9cd21d41e339b4d1bf8bf859fd3711f32ce4c3
Paul Miller <paul@jettero.pl>
2019-04-15 10:51:01 -0400

    send all buckets to the hstatus module; filter them by last_t

--- hubblestack/extmods/modules/hstatus.py
+++ hubblestack/extmods/modules/hstatus.py
@@ -3,6 +3,7 @@ import re
 import logging
 import math
 import hubblestack.status
+import time
 
 log = logging.getLogger(__name__)
 
@@ -10,6 +11,7 @@ __virtualname__ = 'hstatus'
 
 SOURCETYPE = 'hubble_hec_summary'
 MSG_COUNTS_PAT = r'hubblestack.hec.obj.input:(?P<stype>[^:]+)'
+_last_send_time = 0 # track the last time we sent something
 
 def __virtual__():
     return True
@@ -24,13 +26,20 @@ def msg_counts(pat=MSG_COUNTS_PAT, emit_self=False, sourcetype=SOURCETYPE):
             sourcetype - the sourcetype for the accounting messages (default: hstatus.SOURCETYPE)
     '''
 
+    # NOTE: any logging in here *will* mess up the summary count of hubble_log
+    # (assuming hubble_log is reporting in and the logs are above the logging
+    # level)
+
     pat = re.compile(pat)
     ret = list() # events to return
     for bucket_set in hubblestack.status.HubbleStatus.short('all'):
         for k,v in bucket_set.iteritems():
             try:
                 # should be at least one count
-                if v['count'] <= 1:
+                if v['count'] < 1:
+                    continue
+                # skip records we probably already sent
+                if v['last_t'] < _last_send_time:
                     continue
             except KeyError as e:
                 continue
@@ -48,6 +57,8 @@ def msg_counts(pat=MSG_COUNTS_PAT, emit_self=False, sourcetype=SOURCETYPE):
                         'send_session_start': int(v['first_t']),
                         'send_session_end': int(math.ceil(v['last_t'])) })
     if ret:
+        global _last_send_time
+        _last_send_time = time.time()
         return { 'sourcetype': sourcetype, 'events': ret }
 
 def dump():

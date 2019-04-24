def1669978b52484487a2a4ad6d9aa2bc8327bc3
Emilia Voicu <emilia.voicu95@gmail.com>
2019-04-24 11:38:33 +0000

    fixed linting issues with splunk_generic_return.py

--- hubblestack/extmods/returners/splunk_generic_return.py
+++ hubblestack/extmods/returners/splunk_generic_return.py
@@ -20,12 +20,60 @@ import time
 import hubblestack.utils.stdrec as stdrec
 from hubblestack.hec import http_event_collector, get_splunk_options
 
-def _get_key(dat, k, d=None):
+def _get_key(dat, key, default_value=None):
+    '''
+    Function that emulates the `dict.pop` behavior.
+    Filters out structures that are not dict.
+
+    dat
+        The dictionary to execute `pop` on
+
+    key
+        The key to be removed
+
+    default_value
+        The value to be returned if `key` is not found in `dat`
+        or if `dat` is not a dict
+    '''
     if isinstance(dat, dict):
-        return dat.pop(k, d)
-    return d
+        return dat.pop(key, default_value)
+
+    return default_value
+
+
+def _build_hec(opts):
+    '''
+    Extract the appropriate parameters from opts,
+    create and return the http_event_collector
+
+    opts
+        dict containing Splunk options to be passed to the `http_event_collector`
+    '''
+    http_event_collector_key = opts['token']
+    http_event_collector_host = opts['indexer']
+    http_event_collector_port = opts['port']
+    hec_ssl = opts['http_event_server_ssl']
+    proxy = opts['proxy']
+    timeout = opts['timeout']
+    http_event_collector_ssl_verify = opts['http_event_collector_ssl_verify']
+
+    hec = http_event_collector(http_event_collector_key, http_event_collector_host,
+                               http_event_port=http_event_collector_port,
+                               http_event_server_ssl=hec_ssl,
+                               http_event_collector_ssl_verify=http_event_collector_ssl_verify,
+                               proxy=proxy, timeout=timeout)
+
+    return hec
+
 
 def returner(retdata):
+    '''
+    Build the event and send it to the http event collector
+    to have it published to Splunk
+
+    retdata
+        A dict containing the data to be returned
+    '''
     try:
         retdata = retdata['return']
     except KeyError:
@@ -33,27 +81,15 @@ def returner(retdata):
 
     opts_list = get_splunk_options()
     for opts in opts_list:
-        http_event_collector_key = opts['token']
-        http_event_collector_host = opts['indexer']
-        http_event_collector_port = opts['port']
-        hec_ssl = opts['http_event_server_ssl']
-        proxy = opts['proxy']
-        timeout = opts['timeout']
-        http_event_collector_ssl_verify = opts['http_event_collector_ssl_verify']
-
-        hec = http_event_collector(http_event_collector_key, http_event_collector_host,
-                                   http_event_port=http_event_collector_port, http_event_server_ssl=hec_ssl,
-                                   http_event_collector_ssl_verify=http_event_collector_ssl_verify,
-                                   proxy=proxy, timeout=timeout)
-
+        hec = _build_hec(opts)
         t_sourcetype = _get_key(retdata, 'sourcetype', 'hubble_generic')
-        t_time       = _get_key(retdata, 'time', time.time())
-        events       = _get_key(retdata, 'event', _get_key(retdata, 'events'))
+        t_time = _get_key(retdata, 'time', time.time())
+        events = _get_key(retdata, 'event', _get_key(retdata, 'events'))
 
         if events is None:
             return
 
-        if not isinstance(events, (list,tuple)):
+        if not isinstance(events, (list, tuple)):
             events = [events]
 
         if len(events) < 1 or (len(events) == 1 and events[0] is None):

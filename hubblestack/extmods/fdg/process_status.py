2d6a85858d4abf1b513c4cd784631f52cebacdc3
Emilia Voicu <emilia.voicu95@gmail.com>
2019-07-09 10:00:21 +0000

    clean process_status.py

--- hubblestack/extmods/fdg/process_status.py
+++ hubblestack/extmods/fdg/process_status.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import
 import logging
 
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
 def list_processes(chained=None, chained_status=None):
@@ -23,8 +23,8 @@ def list_processes(chained=None, chained_status=None):
     res = _run_query('SELECT name FROM processes')
     try:
         ret = _convert_to_str(res['data'])
-    except (KeyError, TypeError) as exc:
-        log.error('Invalid data type returned by osquery call {0}: {1}'.format(res, exc))
+    except (KeyError, TypeError):
+        LOG.error('Invalid data type returned by osquery call %s.', res, exc_info=True)
         return False, None
 
     return bool(ret), ret
@@ -44,8 +44,8 @@ def _convert_to_str(process_data):
         for process in process_data:
             str_process = {str(name): str(val) for name, val in process.iteritems()}
             ret.append(str_process)
-    except (TypeError, AttributeError) as exc:
-        log.error('Invalid argument type; must be list of dicts: {}'.format(exc))
+    except (TypeError, AttributeError):
+        LOG.error('Invalid argument type; must be list of dicts.', exc_info=True)
         return None
 
     return ret
@@ -61,10 +61,10 @@ def _run_query(query_sql):
     res = __salt__['nebula.query'](query_sql)
     try:
         if not res['result']:
-            log.error("Error executing the osquery query: {}".format(res['error']))
+            LOG.error("Error executing the osquery query: %s", res['error'])
             return None
-    except (KeyError, TypeError) as exc:
-        log.error('Invalid data type returned by osquery call {0}: {1}'.format(res, exc))
+    except (KeyError, TypeError):
+        LOG.error('Invalid data type returned by osquery call %s.', res, exc_info=True)
         return None
 
     return res
@@ -89,9 +89,9 @@ def find_process(filter_sql, fields=None, format_chained=True, chained=None, cha
     field
         String specifying extra fields to be returned about the processes, separated by comma.
         All possible fields:
-            path,cmdline,state,cwd,root,uid,gid,euid,egid,suid,sgid,on_disk,wired_size,resident_size,
-            total_size,user_time,system_time,disk_bytes_read,disk_bytes_written,start_time,
-            parent,pgroup,threads,nice
+            path,cmdline,state,cwd,root,uid,gid,euid,egid,suid,sgid,on_disk,wired_size,
+            resident_size,total_size,user_time,system_time,disk_bytes_read,disk_bytes_written,
+            start_time,parent,pgroup,threads,nice
         If nothing is passed, it will return only the name and PID.
         Pass '*' to select all possible fields.
 
@@ -102,13 +102,15 @@ def find_process(filter_sql, fields=None, format_chained=True, chained=None, cha
         The value chained from the previous call.
 
     ``note``
-        If no processes matched the filter or an invalid field is passed, the function returns an empty list.
+        If no processes matched the filter or an invalid field is passed,
+        the function returns an empty list.
     """
-    if format_chained:
-        try:
-            filter_sql = filter_sql.format(chained)
-        except (AttributeError, TypeError) as exc:
-            log.error("Invalid arguments: {}".format(exc))
+    if chained_status:
+        if format_chained:
+            try:
+                filter_sql = filter_sql.format(chained)
+            except (AttributeError, TypeError):
+                LOG.error("Invalid arguments.", exc_info=True)
     # default fields to `name` and `PID`
     if fields:
         fields += ',name,pid'
@@ -118,9 +120,8 @@ def find_process(filter_sql, fields=None, format_chained=True, chained=None, cha
     res = _run_query(query)
     try:
         ret = _convert_to_str(res['data'])
-    except (KeyError, TypeError) as exc:
-        log.error('Invalid data type returned by osquery call {0}: {1}'.format(
-            res, exc))
+    except (KeyError, TypeError):
+        LOG.error('Invalid data type returned by osquery call %s.', res, exc_info=True)
         return False, None
 
     return bool(ret), ret
@@ -145,20 +146,22 @@ def is_running(filter_sql, format_chained=True, chained=None, chained_status=Non
         The value chained from the previous call.
 
     ``note``
-        If more than one process matches the search, the function returns `False` and reports an error.
+        If more than one process matches the search, the function returns `False`
+         and reports an error.
     """
-    if format_chained:
-        try:
-            filter_sql = filter_sql.format(chained)
-        except (AttributeError, TypeError) as exc:
-            log.error('Invalid arguments: {}'.format(exc))
+    if chained_status:
+        if format_chained:
+            try:
+                filter_sql = filter_sql.format(chained)
+            except (AttributeError, TypeError):
+                LOG.error('Invalid arguments.', exc_info=True)
     query = 'SELECT state FROM processes where {0}'.format(filter_sql)
     res = _run_query(query)
     if not res:
         return False, None
     # more than one process
     if len(res['data']) > 1:
-        log.error('Search outputs {} results. Should output only 1'.format(len(res['data'])))
+        LOG.error('Search outputs %d results. Should output only 1', len(res['data']))
         return False, None
     # no processses matched the search
     if not res['data']:
@@ -170,7 +173,8 @@ def is_running(filter_sql, format_chained=True, chained=None, chained_status=Non
     return True, False
 
 
-def find_children(parent_filter, parent_field=None, returned_fields=None, format_chained=False, chained=None, chained_status=None):
+def find_children(parent_filter, parent_field=None, returned_fields=None,
+                  format_chained=False, chained=None, chained_status=None):
     """
     Returns a list of processes (dict with process data) that match the filter criteria.
 
@@ -194,12 +198,13 @@ def find_children(parent_filter, parent_field=None, returned_fields=None, format
         total_size,user_time,system_time,disk_bytes_read,disk_bytes_written,start_time,
         parent,pgroup,threads,nice
     """
-    if format_chained:
-        try:
-            parent_filter = parent_filter.format(chained)
-        except (AttributeError, TypeError) as exc:
-            log.error('Invalid arguments: {0}'.format(exc))
-            return False, None
+    if chained_status:
+        if format_chained:
+            try:
+                parent_filter = parent_filter.format(chained)
+            except (AttributeError, TypeError):
+                LOG.error('Invalid arguments.', exc_info=True)
+                return False, None
     # default returned_fields to `name` and `PID`
     if returned_fields:
         returned_fields += ',name,pid'
@@ -208,13 +213,14 @@ def find_children(parent_filter, parent_field=None, returned_fields=None, format
     # default parent_field to `name`
     if not parent_field:
         parent_field = 'name'
-    query = "SELECT {0} FROM processes WHERE parent == (SELECT pid FROM processes WHERE {1} == '{2}')".format(
-        returned_fields, parent_field, parent_filter)
+    query = "SELECT {0} FROM processes WHERE parent == " \
+            "(SELECT pid FROM processes WHERE {1} == '{2}')".format(
+                returned_fields, parent_field, parent_filter)
     res = _run_query(query)
     try:
         ret = _convert_to_str(res['data'])
-    except (KeyError, TypeError) as exc:
-        log.error('Invalid data type returned by osquery call {0}: {1}'.format(res, exc))
+    except (KeyError, TypeError):
+        LOG.error('Invalid data type returned by osquery call %s.', res, exc_info=True)
         return False, None
 
     return bool(ret), ret

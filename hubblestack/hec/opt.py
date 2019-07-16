a707006358367e67311b14e33ba393d8b9e5ef66
yassingh <yassingh@adobe.com>
2019-07-16 18:11:28 +0530

    changing a comment

--- hubblestack/hec/opt.py
+++ hubblestack/hec/opt.py
@@ -29,7 +29,7 @@ class Required(object):
 REQUIRED = Required()
 del Required
 
-MODALITIES = ('grains.get','config.get',) # config.get does not update splunk config on runtime
+MODALITIES = ('grains.get','config.get',) # search in grains first, fallback to config.get
 
 def _get_splunk_options(space, modality, **kw):
     ret = list()

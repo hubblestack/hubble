d27b92c7d17bf9eab5fd6182b8a8f32503d7a95e
Emilia Voicu <emilia.voicu95@gmail.com>
2019-07-18 13:13:37 +0000

    fix linting for hubble.py

--- hubblestack/extmods/modules/hubble.py
+++ hubblestack/extmods/modules/hubble.py
@@ -12,24 +12,20 @@ Configuration:
     - hubblestack:nova:autosync
 """
 from __future__ import absolute_import
+
 import logging
-
-log = logging.getLogger(__name__)
-
-import imp
 import os
-import sys
-import six
-import inspect
-import yaml
 import traceback
+import yaml
 
-import salt
-import salt.utils
 from salt.exceptions import CommandExecutionError
 from hubblestack import __version__
 from hubblestack.status import HubbleStatus
+
+LOG = logging.getLogger(__name__)
+
 hubble_status = HubbleStatus(__name__, 'top', 'audit')
+
 try:
     from nova_loader import NovaLazyLoader
 except ImportError:
@@ -118,7 +114,7 @@ def audit(configs=None,
                    labels=labels)
     if labels:
         if not isinstance(labels, list):
-            labels=labels.split(',')
+            labels = labels.split(',')
     if not called_from_top and __salt__['config.get']('hubblestack:nova:autoload', True):
         load()
     if not __nova__:
@@ -131,7 +127,7 @@ def audit(configs=None,
     if show_compliance is None:
         show_compliance = __salt__['config.get']('hubblestack:nova:show_compliance', True)
     if show_profile is not None:
-        log.warning(
+        LOG.warning(
             'Keyword argument \'show_profile\' is no longer supported'
         )
     if debug is None:
@@ -146,6 +142,48 @@ def audit(configs=None,
                for con in configs]
 
     # Pass any module parameters through to the Nova module
+    nova_kwargs = _get_nova_kwargs(**kwargs)
+
+    LOG.debug('nova_kwargs: %s', str(nova_kwargs))
+
+    ret = _run_audit(configs, tags, debug, labels, **nova_kwargs)
+    results = _build_results(verbose, ret, show_success, show_compliance, called_from_top)
+
+    return results
+
+
+def _build_results(verbose, ret, show_success, show_compliance, called_from_top):
+    """
+    Helper function that builds the results to be returned depending
+    on whether it was called with verbose or not.
+    """
+    terse_results, compliance = _build_terse_results(ret, show_success, show_compliance)
+
+    # Format verbose output as single-key dictionaries with tag as key
+    if verbose:
+        results = _build_verbose_results(ret, show_success)
+    else:
+        results = terse_results
+
+    if compliance:
+        results['Compliance'] = compliance
+
+    if not called_from_top and not results:
+        results['Messages'] = 'No audits matched this host in the specified profiles.'
+
+    for error in ret.get('Errors', []):
+        if 'Errors' not in results:
+            results['Errors'] = []
+        results['Errors'].append(error)
+
+    return results
+
+
+def _get_nova_kwargs(**kwargs):
+    """
+    Helper function that builds the parameters to be passed to
+    the Nova module in the form of kwargs.
+    """
     nova_kwargs = {}
     # Get values from config first (if any) and merge into nova_kwargs
     nova_kwargs_config = __salt__['config.get']('hubblestack:nova:nova_kwargs', False)
@@ -155,15 +193,16 @@ def audit(configs=None,
     if kwargs is not None:
         nova_kwargs.update(kwargs)
 
-    log.debug('nova_kwargs: ' + str(nova_kwargs))
+    return nova_kwargs
 
-    ret = _run_audit(configs, tags, debug, labels, **nova_kwargs)
 
-    terse_results = {}
-    verbose_results = {}
+def _build_terse_results(ret, show_success, show_compliance):
+    """
+    Helper function that builds the results to be returned when the verbose parameter is not set
+    """
+    terse_results = {'Failure': []}
 
     # Pull out just the tag and description
-    terse_results['Failure'] = []
     tags_descriptions = set()
 
     for tag_data in ret.get('Failure', []):
@@ -206,55 +245,106 @@ def audit(configs=None,
     if not terse_results['Controlled']:
         terse_results.pop('Controlled')
 
-    # Format verbose output as single-key dictionaries with tag as key
-    if verbose:
-        verbose_results['Failure'] = []
+    return terse_results, compliance
 
-        for tag_data in ret.get('Failure', []):
-            tag = tag_data['tag']
-            verbose_results['Failure'].append({tag: tag_data})
 
-        verbose_results['Success'] = []
+def _build_verbose_results(ret, show_success):
+    """
+    Helper function that builds the results to be returned when the verbose parameter is set
+    """
+    verbose_results = {'Failure': []}
 
-        for tag_data in ret.get('Success', []):
-            tag = tag_data['tag']
-            verbose_results['Success'].append({tag: tag_data})
+    for tag_data in ret.get('Failure', []):
+        tag = tag_data['tag']
+        verbose_results['Failure'].append({tag: tag_data})
 
-        if not show_success and 'Success' in verbose_results:
-            verbose_results.pop('Success')
+    verbose_results['Success'] = []
 
-        verbose_results['Controlled'] = []
+    for tag_data in ret.get('Success', []):
+        tag = tag_data['tag']
+        verbose_results['Success'].append({tag: tag_data})
 
-        for tag_data in ret.get('Controlled', []):
-            tag = tag_data['tag']
-            verbose_results['Controlled'].append({tag: tag_data})
+    if not show_success and 'Success' in verbose_results:
+        verbose_results.pop('Success')
 
-        if not verbose_results['Controlled']:
-            verbose_results.pop('Controlled')
+    verbose_results['Controlled'] = []
 
-        results = verbose_results
-    else:
-        results = terse_results
+    for tag_data in ret.get('Controlled', []):
+        tag = tag_data['tag']
+        verbose_results['Controlled'].append({tag: tag_data})
 
-    if compliance:
-        results['Compliance'] = compliance
+    if not verbose_results['Controlled']:
+        verbose_results.pop('Controlled')
 
-    if not called_from_top and not results:
-        results['Messages'] = 'No audits matched this host in the specified profiles.'
+    return verbose_results
 
-    for error in ret.get('Errors', []):
-        if 'Errors' not in results:
-            results['Errors'] = []
-        results['Errors'].append(error)
+
+def _run_audit(configs, tags, debug, labels, **kwargs):
+    """
+    Function that runs the audits that need to be run based on the configs.
+    """
+    results = {}
+
+    # compile list of tuples with profile name and profile data
+    data_list = _build_audit_data(configs, results)
+
+    if debug:
+        LOG.debug('hubble.py configs:')
+        LOG.debug(configs)
+        LOG.debug('hubble.py data_list:')
+        LOG.debug(data_list)
+    # Run the audits
+    # This is currently pretty brute-force -- we just run all the modules we
+    # have available with the data list, so data will be processed multiple
+    # times. However, for the scale we're working at this should be fine.
+    # We can revisit if this ever becomes a big bottleneck
+    for key, func in __nova__._dict.iteritems():
+        try:
+            ret = func(data_list, tags, labels, **kwargs)
+        except Exception:
+            LOG.error('Exception occurred in nova module:')
+            LOG.error(traceback.format_exc())
+            if 'Errors' not in results:
+                results['Errors'] = []
+            results['Errors'].append({key: {'error': 'exception occurred',
+                                            'data': traceback.format_exc().splitlines()[-1]}})
+            continue
+        else:
+            if not isinstance(ret, dict):
+                if 'Errors' not in results:
+                    results['Errors'] = []
+                results['Errors'].append({key: {'error': 'bad return type',
+                                                'data': ret}})
+                continue
+
+        # Merge in the results
+        for ret_key, ret_val in ret.iteritems():
+            if ret_key not in results:
+                results[ret_key] = []
+            results[ret_key].extend(ret_val)
+
+    # Inspect the data for compensating control data
+    processed_controls = _build_processed_controls(data_list, debug)
+
+    # Look through the failed results to find audits which match our control config
+    failures_to_remove = _build_failures_to_remove(results, processed_controls)
+
+    # Remove controlled failures from results['Failure']
+    for failure_index in reversed(sorted(set(failures_to_remove))):
+        results['Failure'].pop(failure_index)
+
+    for key in results.keys():
+        if not results[key]:
+            results.pop(key)
 
     return results
 
 
-def _run_audit(configs, tags, debug, labels, **kwargs):
-
-    results = {}
-
-    # Compile a list of audit data sets which we need to run
+def _build_audit_data(configs, results):
+    """
+    Helper function that goes over each config and extract the audit data sets
+    that need to be run.
+    """
     to_run = set()
     for config in configs:
         found_for_config = False
@@ -273,49 +363,18 @@ def _run_audit(configs, tags, debug, labels, **kwargs):
             # No matches were found for this entry, add an error
             if 'Errors' not in results:
                 results['Errors'] = []
-            results['Errors'].append({config: {'error': 'No matching profiles found for {0}'
-                                                        .format(config)}})
+            results['Errors'].append(
+                {config: {'error': 'No matching profiles found for {0}'.format(config)}})
 
-    # compile list of tuples with profile name and profile data
-    data_list = [(key.split('.yaml')[0].split(os.path.sep)[-1],
-                  __nova__.__data__[key]) for key in to_run]
-    if debug:
-        log.debug('hubble.py configs:')
-        log.debug(configs)
-        log.debug('hubble.py data_list:')
-        log.debug(data_list)
-    # Run the audits
-    # This is currently pretty brute-force -- we just run all the modules we
-    # have available with the data list, so data will be processed multiple
-    # times. However, for the scale we're working at this should be fine.
-    # We can revisit if this ever becomes a big bottleneck
-    for key, func in __nova__._dict.iteritems():
-        try:
-            ret = func(data_list, tags, labels, **kwargs)
-        except Exception as exc:
-            log.error('Exception occurred in nova module:')
-            log.error(traceback.format_exc())
-            if 'Errors' not in results:
-                results['Errors'] = []
-            results['Errors'].append({key: {'error': 'exception occurred',
-                                            'data': traceback.format_exc().splitlines()[-1]}})
-            continue
-        else:
-            if not isinstance(ret, dict):
-                if 'Errors' not in results:
-                    results['Errors'] = []
-                results['Errors'].append({key: {'error': 'bad return type',
-                                                'data': ret}})
-                continue
+    return [(key.split('.yaml')[0].split(os.path.sep)[-1],
+             __nova__.__data__[key]) for key in to_run]
 
-        # Merge in the results
-        for key, val in ret.iteritems():
-            if key not in results:
-                results[key] = []
-            results[key].extend(val)
 
+def _build_processed_controls(data_list, debug):
+    """
+    Helper function that builds a dictionary containing compensating control data
+    """
     processed_controls = {}
-    # Inspect the data for compensating control data
     for _, audit_data in data_list:
         control_config = audit_data.get('control', [])
         for control in control_config:
@@ -327,12 +386,18 @@ def _run_audit(configs, tags, debug, labels, **kwargs):
                         processed_controls[control_tag] = {'reason': control_data}
                     else:  # dict
                         processed_controls[control_tag] = control_data
-
     if debug:
-        log.debug('hubble.py control data:')
-        log.debug(processed_controls)
+        LOG.debug('hubble.py control data:')
+        LOG.debug(processed_controls)
 
-    # Look through the failed results to find audits which match our control config
+    return processed_controls
+
+
+def _build_failures_to_remove(results, processed_controls):
+    """
+    Helper function that goes over failed audits and looks for ones that
+    match the control config.
+    """
     failures_to_remove = []
     for i, failure in enumerate(results.get('Failure', [])):
         failure_tag = failure['tag']
@@ -345,16 +410,7 @@ def _run_audit(configs, tags, debug, labels, **kwargs):
             })
             results['Controlled'].append(failure)
 
-    # Remove controlled failures from results['Failure']
-    if failures_to_remove:
-        for failure_index in reversed(sorted(set(failures_to_remove))):
-            results['Failure'].pop(failure_index)
-
-    for key in results.keys():
-        if not results[key]:
-            results.pop(key)
-
-    return results
+    return failures_to_remove
 
 
 @hubble_status.watch
@@ -363,7 +419,6 @@ def top(topfile='top.nova',
         show_success=None,
         show_compliance=None,
         show_profile=None,
-        debug=None,
         labels=None):
     """
     Compile and run all yaml data from the specified nova topfile.
@@ -446,40 +501,17 @@ def top(topfile='top.nova',
     if show_compliance is None:
         show_compliance = __salt__['config.get']('hubblestack:nova:show_compliance', True)
     if show_profile is not None:
-        log.warning(
+        LOG.warning(
             'Keyword argument \'show_profile\' is no longer supported'
         )
-    if debug is None:
-        debug = __salt__['config.get']('hubblestack:nova:debug', False)
 
     results = {}
-
-    # Get a list of yaml to run
-    top_data = _get_top_data(topfile)
 
     # Will be a combination of strings and single-item dicts. The strings
     # have no tag filters, so we'll treat them as tag filter '*'. If we sort
     # all the data by tag filter we can batch where possible under the same
     # tag.
-    data_by_tag = {}
-    for data in top_data:
-        if isinstance(data, basestring):
-            if '*' not in data_by_tag:
-                data_by_tag['*'] = []
-            data_by_tag['*'].append(data)
-        elif isinstance(data, dict):
-            for key, tag in data.iteritems():
-                if tag not in data_by_tag:
-                    data_by_tag[tag] = []
-                data_by_tag[tag].append(key)
-        else:
-            if 'Errors' not in results:
-                results['Errors'] = {}
-            error_log = 'topfile malformed, list entries must be strings or '\
-                        'dicts: {0} | {1}'.format(data, type(data))
-            results['Errors'][topfile] = {'error': error_log}
-            log.error(error_log)
-            continue
+    data_by_tag = _build_data_by_tag(topfile, results)
 
     if not data_by_tag:
         return results
@@ -505,6 +537,50 @@ def top(topfile='top.nova',
         if compliance:
             results['Compliance'] = compliance
 
+    _clean_up_results(results, show_success)
+
+    return results
+
+
+def _build_data_by_tag(topfile, results):
+    """
+    Helper function that goes over data in top_data and
+    aggregate it by tag
+    """
+    data_by_tag = {}
+
+    # Get a list of yaml to run
+    top_data = _get_top_data(topfile)
+
+    for data in top_data:
+        if isinstance(data, basestring):
+            if '*' not in data_by_tag:
+                data_by_tag['*'] = []
+            data_by_tag['*'].append(data)
+        elif isinstance(data, dict):
+            for key, tag in data.iteritems():
+                if tag not in data_by_tag:
+                    data_by_tag[tag] = []
+                data_by_tag[tag].append(key)
+        else:
+            if 'Errors' not in results:
+                results['Errors'] = {}
+            error_log = 'topfile malformed, list entries must be strings or ' \
+                        'dicts: {0} | {1}'.format(data, type(data))
+            results['Errors'][topfile] = {'error': error_log}
+            LOG.error(error_log)
+            continue
+
+    return data_by_tag
+
+
+def _clean_up_results(results, show_success):
+    """
+    Helper function that cleans up the results by
+    removing the keys with empty values, removing `success`
+    if show_success was not passed, adding an error message if
+    results is empty
+    """
     for key in results.keys():
         if not results[key]:
             results.pop(key)
@@ -514,8 +590,6 @@ def top(topfile='top.nova',
 
     if not show_success and 'Success' in results:
         results.pop('Success')
-
-    return results
 
 
 def sync(clean=False):
@@ -547,10 +621,10 @@ def sync(clean=False):
         salt '*' nova.sync
         salt '*' nova.sync saltenv=hubble
     """
-    log.debug('syncing nova modules')
+    LOG.debug('syncing nova modules')
     nova_profile_dir = __salt__['config.get']('hubblestack:nova:profile_dir',
                                               'salt://hubblestack_nova_profiles')
-    nova_module_dir, cached_profile_dir = _hubble_dir()
+    _nova_module_dir, cached_profile_dir = _hubble_dir()
     saltenv = __salt__['config.get']('hubblestack:nova:saltenv', 'base')
 
     # Clean previously synced files
@@ -596,7 +670,7 @@ def load():
         if not os.path.isdir(nova_dir):
             return False, 'No synced nova modules/profiles found'
 
-    log.debug('loading nova modules')
+    LOG.debug('loading nova modules')
 
     global __nova__
     __nova__ = NovaLazyLoader(_hubble_dir(), __opts__, __grains__, __pillar__, __salt__)
@@ -664,11 +738,11 @@ def _get_top_data(topfile):
     try:
         with open(topfile) as handle:
             topdata = yaml.safe_load(handle)
-    except Exception as e:
-        raise CommandExecutionError('Could not load topfile: {0}'.format(e))
+    except Exception as exc:
+        raise CommandExecutionError('Could not load topfile: {0}'.format(exc))
 
     if not isinstance(topdata, dict) or 'nova' not in topdata or \
-            not(isinstance(topdata['nova'], dict)):
+            (not isinstance(topdata['nova'], dict)):
         raise CommandExecutionError('Nova topfile not formatted correctly')
 
     topdata = topdata['nova']

0bc057962bfdc729325b57d3e16a279d77c66c1a
Chandler Newby <chandler.newby@gmail.com>
2019-07-19 17:59:07 -0600

    Do error checking on the 'host' value in splunk_generic_return.py

--- hubblestack/utils/stdrec.py
+++ hubblestack/utils/stdrec.py
@@ -13,8 +13,44 @@ def std_info():
           minion_id, dest_host, dest_ip, dest_fqdn and system_uuid
     """
     minion_id = __opts__['id']
+    local_fqdn = __grains__.get('local_fqdn', __grains__['fqdn'])
+
+    ret = {
+        'minion_id': minion_id,
+        'dest_host': get_fqdn(),
+        'dest_ip': get_fqdn_ip4(),
+        'dest_fqdn': local_fqdn,
+        'system_uuid': __grains__.get('system_uuid')
+    }
+
+    ret.update(__grains__.get('cloud_details', {}))
+
+    return ret
+
+
+def get_fqdn():
+    """
+    Do lots of error checking and get as close to a useable fqdn as possible
+    """
+    minion_id = __opts__['id']
     fqdn = __grains__['fqdn']
     fqdn = fqdn if fqdn else minion_id
+
+    # Sometimes fqdn reports a value of localhost. If that happens, try another method.
+    bad_fqdns = ['localhost', 'localhost.localdomain', 'localhost6.localdomain6']
+    if fqdn in bad_fqdns:
+        new_fqdn = socket.gethostname()
+        if '.' not in new_fqdn or new_fqdn in bad_fqdns:
+            new_fqdn = get_fqdn_ip4()
+        fqdn = new_fqdn
+
+    return fqdn
+
+
+def get_fqdn_ip4():
+    """
+    Get the first non-127.0* address as the fqdn ip
+    """
     try:
         fqdn_ip4 = __grains__.get('local_ip4')
         if not fqdn_ip4:
@@ -29,27 +65,9 @@ def std_info():
             if ip4_addr and not ip4_addr.startswith('127.'):
                 fqdn_ip4 = ip4_addr
                 break
-    local_fqdn = __grains__.get('local_fqdn', __grains__['fqdn'])
 
-    # Sometimes fqdn reports a value of localhost. If that happens, try another method.
-    bad_fqdns = ['localhost', 'localhost.localdomain', 'localhost6.localdomain6']
-    if fqdn in bad_fqdns:
-        new_fqdn = socket.gethostname()
-        if '.' not in new_fqdn or new_fqdn in bad_fqdns:
-            new_fqdn = fqdn_ip4
-        fqdn = new_fqdn
+    return fqdn_ip4
 
-    ret = {
-        'minion_id': minion_id,
-        'dest_host': fqdn,
-        'dest_ip': fqdn_ip4,
-        'dest_fqdn': local_fqdn,
-        'system_uuid': __grains__.get('system_uuid')
-    }
-
-    ret.update(__grains__.get('cloud_details', {}))
-
-    return ret
 
 def index_extracted(payload):
     """ generate index extracted fields dictionary from the given payload based
@@ -69,6 +87,7 @@ def index_extracted(payload):
             if not isinstance(val, (list, dict, tuple)):
                 fields["meta_%s" % item] = str(val)
     return fields
+
 
 def update_payload(payload):
     """ update the given payload with index extracted fields (if applicable)

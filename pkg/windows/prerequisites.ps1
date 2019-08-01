3ec497c9b87902655e02a9bd06bd4dddcaef8132
Prakhar Sinha <prsinha@adobe.com>
2019-08-01 12:22:01 +0530

    Handling scenarios where choco install would fail

--- pkg/windows/prerequisites.ps1
+++ pkg/windows/prerequisites.ps1
@@ -11,7 +11,18 @@ else {
         $ChocoInstalled = $true
     }
     else {
-        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+		$securityProtocolSettingsOriginal = [System.Net.ServicePointManager]::SecurityProtocol
+
+		try {
+		  # This should work in .NET 4 where .NET 4.5 is installed as an inplace upgrade
+		  # Set TLS1.2 (3072) then TLS1.1 (768), then TLS 1.0 (192), finally SSL3 (48)
+		  $securityProtocolSettings = 3072 -bor 768 -bor 192 -bor 48 
+		  [System.Net.ServicePointManager]::SecurityProtocol = $securityProtocolSettings
+		} catch {
+		  Write-Warning "Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. Please upgrade to at least .NET Framework 4.5 and PowerShell v3 for this to work appropriately."
+		}
+		Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+		[System.Net.ServicePointManager]::SecurityProtocol = $securityProtocolSettingsOriginal
     }
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex $InstallGitCommand
 }

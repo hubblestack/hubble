89d80315f14afa2bde338ab5899e5521a1b20264
Emilia Voicu <emilia.voicu95@gmail.com>
2019-09-10 16:01:19 +0000

    clean graylog_nebula_return

--- hubblestack/extmods/returners/graylog_nebula_return.py
+++ hubblestack/extmods/returners/graylog_nebula_return.py
@@ -24,70 +24,72 @@ hubblestack:
 from __future__ import absolute_import
 
 import json
-import time
 import requests
-from datetime import datetime
 import hubblestack.extmods.returners.common.graylog as graylog
 
 
 def returner(ret):
     """
+    Aggregates the configuration options related to graylog and returns a dict containing them.
     """
+    # sanity check
+    if not ret['return']:
+        return
+
     opts_list = graylog.get_options('nebula')
 
     # Get cloud details
     cloud_details = __grains__.get('cloud_details', {})
 
+    fqdn = __grains__['fqdn'] if __grains__['fqdn'] else ret['id']
+    try:
+        fqdn_ip4 = __grains__['fqdn_ip4'][0]
+    except IndexError:
+        fqdn_ip4 = __grains__['ipv4'][0]
+    if fqdn_ip4.startswith('127.'):
+        for ip4_addr in __grains__['ipv4']:
+            if ip4_addr and not ip4_addr.startswith('127.'):
+                fqdn_ip4 = ip4_addr
+                break
+
     for opts in opts_list:
-        custom_fields = opts['custom_fields']
-        gelfhttp = opts['gelfhttp']
-        port = opts['port']
-        # assign all the things
-        data = ret['return']
-        minion_id = ret['id']
 
-        fqdn = __grains__['fqdn']
-        fqdn = fqdn if fqdn else minion_id
-        try:
-            fqdn_ip4 = __grains__['fqdn_ip4'][0]
-        except IndexError:
-            fqdn_ip4 = __grains__['ipv4'][0]
-        if fqdn_ip4.startswith('127.'):
-            for ip4_addr in __grains__['ipv4']:
-                if ip4_addr and not ip4_addr.startswith('127.'):
-                    fqdn_ip4 = ip4_addr
-                    break
-
-        if not data:
-            return
-
-        for query in data:
+        for query in ret['return']:
             for query_name, value in query.items():
-                for d in value['data']:
-                    payload = {}
-                    event = {}
-                    event.update(d)
-                    event.update({'query': query_name})
-                    event.update({'job_id': ret['jid']})
-                    event.update({'minion_id': minion_id})
-                    event.update({'dest_host': fqdn})
-                    event.update({'dest_ip': fqdn_ip4})
-                    event.update(cloud_details)
+                for query_data in value['data']:
+                    args = {'query': query_name,
+                            'job_id': ret['jid'],
+                            'minion_id': ret['id'],
+                            'dest_host': fqdn,
+                            'dest_ip': fqdn_ip4}
+                    event = _generate_event(opts['custom_fields'], args, cloud_details, query_data)
 
-                    for custom_field in custom_fields:
-                        custom_field_name = 'custom_' + custom_field
-                        custom_field_value = __salt__['config.get'](custom_field, '')
-                        if isinstance(custom_field_value, str):
-                            event.update({custom_field_name: custom_field_value})
-                        elif isinstance(custom_field_value, list):
-                            custom_field_value = ','.join(custom_field_value)
-                            event.update({custom_field_name: custom_field_value})
-
-                    payload.update({'host': fqdn})
-                    payload.update({'_sourcetype': opts['sourcetype']})
-                    payload.update({'short_message': 'hubblestack'})
-                    payload.update({'hubblemsg': event})
+                    payload = {'host': fqdn,
+                               '_sourcetype': opts['sourcetype'],
+                               'short_message': 'hubblestack',
+                               'hubblemsg': event}
 
                     rdy = json.dumps(payload)
-                    requests.post('{}:{}/gelf'.format(gelfhttp, port), rdy)
+                    requests.post('{}:{}/gelf'.format(opts['gelfhttp'], opts['port']), rdy)
     return
+
+
+def _generate_event(custom_fields, args, cloud_details, query_data):
+    """
+    Helper function that builds and returns the event dict
+    """
+    event = {}
+    event.update(query_data)
+    event.update(args)
+    event.update(cloud_details)
+
+    for custom_field in custom_fields:
+        custom_field_name = 'custom_' + custom_field
+        custom_field_value = __salt__['config.get'](custom_field, '')
+        if isinstance(custom_field_value, str):
+            event.update({custom_field_name: custom_field_value})
+        elif isinstance(custom_field_value, list):
+            custom_field_value = ','.join(custom_field_value)
+            event.update({custom_field_name: custom_field_value})
+
+    return event

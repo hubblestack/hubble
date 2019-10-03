4e129345eeff08978bf476206a8902c807787da5
Emilia Voicu <emilia.voicu95@gmail.com>
2019-10-03 16:07:01 +0000

    fix bug in dedup_list to allow for list of dicts

--- hubblestack/extmods/returners/splunk_pulsar_return.py
+++ hubblestack/extmods/returners/splunk_pulsar_return.py
@@ -109,7 +109,11 @@ def _dedup_list(input_list):
     """
     Function that removes duplicates from a list
     """
-    return list(set(input_list))
+    deduped = []
+    for idx, item in enumerate(input_list):
+        if item not in input_list[idx + 1:]:
+            deduped.append(item)
+    return deduped
 
 
 def _build_linux_actions():

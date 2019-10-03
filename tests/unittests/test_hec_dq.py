cf226611f347762f98f9b074ac756ad2b22f8c69
Paul Miller <paul@jettero.pl>
2019-10-03 07:33:22 -0400

    forgot to adjust test for meta_data

--- tests/unittests/test_hec_dq.py
+++ tests/unittests/test_hec_dq.py
@@ -17,31 +17,31 @@ def dq():
 def test_disk_queue(dq):
     borked = False
 
-    dq.put(b'one')
-    dq.put(b'two')
-    dq.put(b'three')
+    dq.put(b'one', testinator=3)
+    dq.put(b'two', testinator=4)
+    dq.put(b'three', testinator=5)
 
     assert len(dq) == 13
-    assert dq.peek() == b'one'
-    assert dq.get() == b'one'
-    assert dq.peek() == b'two'
+    assert dq.peek() == (b'one', {'testinator': 3})
+    assert dq.get() == (b'one', {'testinator': 3})
+    assert dq.peek() == (b'two', {'testinator': 4})
     assert len(dq) == 9
 
-    assert dq.getz() == b'two three'
+    assert dq.getz() == (b'two three', {'testinator': 5})
     assert len(dq) == 0
 
     dq.put(b'one')
     dq.put(b'two')
     dq.put(b'three')
 
-    assert dq.getz(8) == b'one two'
-    assert dq.getz(8) == b'three'
+    assert dq.getz(8) == (b'one two', {})
+    assert dq.getz(8) == (b'three', {})
 
 def _test_pop(samp,q):
     for i in samp:
         q.put(i)
     for i in samp:
-        assert q.peek() == i
+        assert q.peek() == (i, {})
         q.pop()
 
 def test_dq_pop(samp,dq):

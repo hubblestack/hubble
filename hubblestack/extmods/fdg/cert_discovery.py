b02e5c1e8f2bcd45a9833014381850d692f1aea6
MoodyMudit <muditagr90@gmail.com>
2019-10-22 18:17:58 +0530

    adding try except block

--- hubblestack/extmods/fdg/cert_discovery.py
+++ hubblestack/extmods/fdg/cert_discovery.py
@@ -20,29 +20,31 @@ def load_certificate(ip, port):
         return {'result':True,'data':c}
 
 def parse_cert(cert, port):
-    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert['data'])
-
-    issuer_components = format_components(x509.get_issuer())
-    not_after = datetime.strptime(x509.get_notAfter().decode('utf-8'), "%Y%m%d%H%M%SZ")
-    not_before = datetime.strptime(x509.get_notBefore().decode('utf-8'), "%Y%m%d%H%M%SZ")
-    subject_components = format_components(x509.get_subject())
-    has_expired = x509.has_expired()
-
-    cert_details = {}
-    cert_details['country_name'] = subject_components.get('C', "None")
-    cert_details['organisation_name'] = subject_components.get('O', "None")
-    cert_details['organisation_unit_name'] = subject_components.get('OU', "None")
-    cert_details['common_name'] = subject_components.get('CN', "None")
-    cert_details['version'] = str(x509.get_version())
-    cert_details['has_expired'] = True if has_expired == 1 else False
-    cert_details['serial_number'] = str(x509.get_serial_number())
-    cert_details['issuer'] = issuer_components.get('CN', "None")
-    cert_details['expiry_date'] = str(not_after)
-    cert_details['issue_date'] = str(not_before)
-    cert_details['signature_algo'] = str(x509.get_signature_algorithm())
-    cert_details['pem_cert'] = str(cert['data'])
-    cert_details['port'] = port
-
+    try:
+        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert['data'])
+        cert_details = {}
+        cert_details['port'] = port
+        if x509.get_issuer():
+            issuer_components = format_components(x509.get_issuer())
+            cert_details['issuer'] = issuer_components.get('CN', "None")
+        if x509.get_subject():
+            subject_components = format_components(x509.get_subject())
+            cert_details['country_name'] = subject_components.get('C', "None")
+            cert_details['organisation_name'] = subject_components.get('O', "None")
+            cert_details['organisation_unit_name'] = subject_components.get('OU', "None")
+            cert_details['common_name'] = subject_components.get('CN', "None")
+        not_after = datetime.strptime(x509.get_notAfter().decode('utf-8'), "%Y%m%d%H%M%SZ")
+        not_before = datetime.strptime(x509.get_notBefore().decode('utf-8'), "%Y%m%d%H%M%SZ")
+        has_expired = x509.has_expired()
+        cert_details['version'] = str(x509.get_version())
+        cert_details['has_expired'] = True if has_expired == 1 else False
+        cert_details['serial_number'] = str(x509.get_serial_number())
+        cert_details['expiry_date'] = str(not_after)
+        cert_details['issue_date'] = str(not_before)
+        cert_details['signature_algo'] = str(x509.get_signature_algorithm())
+        cert_details['pem_cert'] = str(cert['data'])
+    except Exception as e:
+        cert_details['error'] = "some error occurred while parsing certificate - {0}".format(e)
     return cert_details
 
 def fill_na(port, message):

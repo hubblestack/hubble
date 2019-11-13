8e142fc58734a8ea87b7e909e1d8484d83acddf5
Paul Miller <paul@jettero.pl>
2019-11-13 08:24:21 -0500

    test

---
+++ tests/unittests/test_fdg_sourcetypes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import hubblestack.extmods.returners.splunk_fdg_return as sfr
+
+def test01():
+    """
+    Exactly how to resolve these sourcetypes caused some discussion:
+        https://github.com/hubblestack/hubble/pull/738
+    Here are the discussed items:
+    """
+    assert sfr._file_url_to_sourcetype('salt://fdg/test.fdg') == 'hubble_fdg_test'
+    assert sfr._file_url_to_sourcetype('salt://////fdg///////test.fdg') == 'hubble_fdg_test'
+    assert sfr._file_url_to_sourcetype('/tmp/file/a/b/c/test.fdg') == 'hubble_fdg_tmp_file_a_b_c_test'
+    assert sfr._file_url_to_sourcetype('/opt/tmpfdg_test/2fdg_cert_test.fdg') == 'hubble_fdg_opt_tmpfdg_test_2fdg_cert_test'

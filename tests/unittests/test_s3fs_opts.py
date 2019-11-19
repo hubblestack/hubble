4e4f52caf540a12396650848e8ed088299f1771b
Paul Miller <paul@jettero.pl>
2019-11-19 18:02:11 -0500

    add a test that shows the old behavior of the opts gathering

---
+++ tests/unittests/test_s3fs_opts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import hubblestack.extmods.fileserver.s3fs as hs_s3fs
+
+def test_s3fs_opts(__opts__):
+    hs_s3fs.__opts__ = __opts__
+    s3fs_opts = hs_s3fs._get_s3_key()
+
+    assert s3fs_opts['https_enable'] is None
+    assert s3fs_opts['verify_ssl']   is None
+    assert s3fs_opts['location']     is None
+    assert s3fs_opts['path_style']   is None
+    assert s3fs_opts['service_url']  is None
+    assert s3fs_opts['keyid']        is None
+    assert s3fs_opts['key']          is None
+    assert s3fs_opts['kms_keyid']    is None
+
+    __opts__['s3.location'] = a = 'us-east-1'
+    __opts__['s3.key'] = b = '1234512345123451234512345123451234512345'
+
+    s3fs_opts = hs_s3fs._get_s3_key()
+    assert s3fs_opts['location'] == a
+    assert s3fs_opts['key']      == b

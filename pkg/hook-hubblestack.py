b0837c3e2d2d83a8f79ec2e60f9445ff24622254
Paul Miller <paul@jettero.pl>
2019-11-20 13:15:28 -0500

    this appears to do the needful

--- pkg/hook-hubblestack.py
+++ pkg/hook-hubblestack.py
@@ -34,6 +34,11 @@ HIDDEN_IMPORTS = [
     'azure.storage.blob',
     'croniter',
     'vulners',
+
+    # fdg readfile.json tries to absolute import a module during lazy load. Too
+    # late for the packer to notice it should be packed in the binary.
+    # marking it here for "hidden import"
+    'hubblestack.utils.encoding',
 ]
 DATAS = []
 binaries = []

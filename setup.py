1563c178e47b18d6a66d95b0f0b8e08e2441b82f
root <root@localhost.localdomain>
2019-12-03 17:25:53 -0700

    platform.linux_distribution & platform.dist replaced with distro

--- setup.py
+++ setup.py
@@ -6,7 +6,14 @@ distro, version, _ = platform.dist()
 if not distro:
     distro, version, _ = platform.linux_distribution(supported_dists=['system'])
 
-# Default to cent7
+    if not distro:
+        try:
+            import distro
+            distro, version, _ = distro.linux_distribution(full_distribution_name=False)
+        except ModuleNotFoundError:
+            distro = version = ''
+
+# Default to CentOS7
 data_files = [('/usr/lib/systemd/system', ['pkg/source/hubble.service']),
               ('/etc/hubble', ['conf/hubble']), ]
 

2a95c66f1c95af9866ede67b6f869d467efa7e73
Prakhar Sinha <prsinha@adobe.com>
2019-12-19 16:04:12 +0530

    Revert "Merge branch 'develop' into 3.0"
    This reverts commit 36927c45db1516b8dad2b58632e7bc1f2a5051e7, reversing
    changes made to 61e52d3328b2037013274cecc73e20a3ff7266be.

--- hubblestack/extmods/grains/splunkconfig.py
+++ hubblestack/extmods/grains/splunkconfig.py
@@ -19,19 +19,18 @@ def splunkconfig():
         ret = _splunkindex(ret)
         return ret
     try:
-        for root, _dirs, files in os.walk(configdir):
-            for conf_file in files:
-                if not conf_file.endswith('.conf'):
-                    pass
-                fpath = os.path.join(root, conf_file)
-                try:
-                    with open(fpath, 'r') as conf_file_handler:
-                        config = yaml.safe_load(conf_file_handler)
-                    if config.get('hubblestack', {}).get('returner', {}).get('splunk'):
-                        ret = {'hubblestack': config['hubblestack']}
-                except Exception:
-                    pass
-    except Exception:
+        for root, dirs, files in os.walk(configdir):
+            for f in files:
+                if f.endswith('.conf'):
+                    fpath = os.path.join(root, f)
+                    try:
+                        with open(fpath, 'r') as fh:
+                            config = yaml.safe_load(fh)
+                        if config.get('hubblestack', {}).get('returner', {}).get('splunk'):
+                            ret = {'hubblestack': config['hubblestack']}
+                    except:
+                        pass
+    except:
         pass
     ret = _splunkindex(ret)
     return ret

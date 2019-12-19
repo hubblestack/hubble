ec00392fe01a96813b504bf2b64381c6a26ef3ab
Prakhar Sinha <prsinha@adobe.com>
2019-12-19 15:52:05 +0530

    Revert "Merge pull request #758 from MoodyMudit/3.0"
    This reverts commit 0f4c6e9cc67ac93f60ca88ef0bdbf792af0141d1, reversing
    changes made to 6c246fcef3d323247cd0eb68ab2091a545be3afe.

--- hubblestack/extmods/grains/splunkconfig.py
+++ hubblestack/extmods/grains/splunkconfig.py
@@ -20,7 +20,7 @@ def splunkconfig():
         return ret
     try:
         for root, _dirs, files in os.walk(configdir):
-            for conf_file in sorted(files):
+            for conf_file in files:
                 if not conf_file.endswith('.conf'):
                     pass
                 fpath = os.path.join(root, conf_file)

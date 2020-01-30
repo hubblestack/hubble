c7c9884c300046abc4c3615ba4ee42265b930a31
Paul Miller <paul@jettero.pl>
2020-01-30 10:53:20 -0500

    not really py3 related test bugfixes

--- tests/unittests/test_hec_obj.py
+++ tests/unittests/test_hec_obj.py
@@ -13,21 +13,28 @@ def test_hec__send_trivially(mock_send):
     hec.sendEvent({'test': 'test-tacular'})
     assert json.loads(mock_send.call_args.args[0].dat)['test'] == 'test-tacular'
 
-@mock.patch.object(HEC, '_send') # just in case, not actually used
+@mock.patch.object(HEC, '_send')
 def test_queue_things_with_compression(mock_send, __opts__, __salt__):
     hec = HEC('token', 'index', 'server',
         disk_queue=TEST_DQ_DIR, disk_queue_size=1000,
         disk_queue_compression=9)
 
+    hec.queue.clear()
+
     results_of_side_effect = list()
-    def side_effect(x):
-        results_of_side_effect.append(x)
+    def side_effect(x, **kw):
+        if 'xxx-mytest-xxx' in x:
+            results_of_side_effect.append(x)
     mock_send.side_effect = side_effect
 
     gz = list()
-    for i in range(100):
-        dat = {f'event{i}': f'test{i}'}
+    for i in range(1):
+        dat = {f'event{i}': f'-xxx-mytest-xxx-test{i}'}
         hec.queueEvent(dat)
         gz.append( json.dumps(dat) )
     hec.flushQueue()
-    assert ' '.join(results_of_side_effect) == ' '.join(gz)
+
+    cat_rez = ' '.join(results_of_side_effect)
+    cat_gz = ' '.join(gz)
+
+    assert cat_rez == cat_gz

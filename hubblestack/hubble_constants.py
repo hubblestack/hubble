66893cf77883c2a2b161cf37b5e6b6f9205244de
Prakhar Sinha <prsinha@adobe.com>
2020-04-20 12:18:16 +0530

    Adding osquery daemon blacklisted flags feature

---
+++ hubblestack/hubble_constants.py
@@ -0,0 +1,16 @@
+# These osquery daemon flags cannot be overridden by remote flagfile stored in file-servers.
+OSQD_BLACKLISTED_FLAGS = ['config_plugin',
+                          # options like filesystem,tls can be provided. As of Hubble 3.0, we always use 'filesystem'
+                          'tls_hostname',
+                          'tls_session_reuse',
+                          'tls_client_cert',
+                          'tls_client_key',
+                          'tls_server_certs',
+                          'disable_enrollmentenroll_secret_path',
+                          'config_tls_endpoint',
+                          'logger_tls_endpoint',
+                          'enrollment_tls_endpoint',
+                          'distributed_tls_read_endpoint',
+                          'distributed_tls_write_endpoint',
+                          'logger_plugin',
+                          'distributed_plugin']

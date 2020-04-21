bd14e81dff4aa6aae7b0b196e44310b4fc3b1bf7
MoodyMudit <muditagr90@gmail.com>
2020-04-22 00:24:01 +0530

    importing osquerylib

--- hubblestack/extmods/grains/docker_details.py
+++ hubblestack/extmods/grains/docker_details.py
@@ -5,7 +5,7 @@ HubbleStack Docker Details Grain
 import salt.modules.cmdmod
 import salt.utils.platform
 import logging
-import hubblestack.utils.osquery_lib as osquery_lib
+from hubblestack.utils.osquery_lib import query as osquery_util
 
 log = logging.getLogger(__name__)
 __salt__ = {'cmd.run': salt.modules.cmdmod._run_quiet,
@@ -38,7 +38,7 @@ def _is_docker_installed():
 def _get_docker_version():
   osquery_path = '/opt/osquery/osqueryi'
   osquery_sql = 'select server_version from docker_info'
-  docker_version = osquery_lib.query(query_sql=osquery_sql, osquery_path=osquery_path)
+  docker_version = osquery_util(query_sql=osquery_sql, osquery_path=osquery_path)
 
   log.debug('docker_version = {0}'.format(docker_version))
 
@@ -48,7 +48,7 @@ def _get_docker_version():
 def _is_docker_process_running():
   osquery_path = '/opt/osquery/osqueryi'
   osquery_sql = 'select name from processes where name LIKE "%docker%"'
-  docker_process = osquery_lib.query(query_sql=osquery_sql, osquery_path=osquery_path)
+  docker_process = osquery_util(query_sql=osquery_sql, osquery_path=osquery_path)
 
   log.debug('docker_process = {0}'.format(docker_process))
 

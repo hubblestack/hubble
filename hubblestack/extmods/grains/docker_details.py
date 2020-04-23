5126c7fdc0b1231fab216eeec8f0b8fbea365a0e
MoodyMudit <muditagr90@gmail.com>
2020-04-23 13:39:31 +0530

    review comments

--- hubblestack/extmods/grains/docker_details.py
+++ hubblestack/extmods/grains/docker_details.py
@@ -34,30 +34,33 @@ def get_docker_details():
     return grains
   except Exception as e:
     log.exception('The following exception occurred while fetching docker details {0}'.format(e))
+    return None
 
 
 def _get_docker_version():
   osquery_sql = 'select server_version from docker_info'
-  docker_version = osquery_util(query_sql=osquery_sql, osquery_path=osquery_path)
+  query_result = osquery_util(query_sql=osquery_sql, osquery_path=osquery_path)
+  if len(query_result) != 0:
+    for result in query_result:
+      if isinstance(result, dict):
+        docker_version = result.get('server_version')
+        log.debug('docker_version = {0}'.format(docker_version))
+        if docker_version and isinstance(docker_version, unicode):
+          return docker_version
 
-  log.debug('docker_version = {0}'.format(docker_version))
-
-  if docker_version and isinstance(docker_version, unicode):
-    return docker_version
-  else:
-    return ''
+  return None
 
 
 def _is_docker_process_running():
-  osquery_sql = 'select name from processes where name LIKE "%dockerd%"'
-  docker_process = osquery_util(query_sql=osquery_sql, osquery_path=osquery_path)
-
-  log.debug('docker_process = {0}'.format(docker_process))
-
-  if 'dockerd' in docker_process:
-    log.info("Docker is running")
-    return True
-  log.info("Docker is not running")
+  osquery_sql = 'select name from processes where name LIKE "%docker%"'
+  docker_process_list = osquery_util(query_sql=osquery_sql, osquery_path=osquery_path)
+  if len(docker_process_list) != 0:
+    for result in docker_process_list:
+      process_name = result.get('name')
+      if 'dockerd' in process_name:
+        log.debug("Docker is running")
+        return True
+  log.debug("Docker is not running")
   return False
 
 

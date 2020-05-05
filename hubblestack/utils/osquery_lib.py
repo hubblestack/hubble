4adf4fc31352252d6a78ec194dc44223c477b9ac
MoodyMudit <muditagr90@gmail.com>
2020-05-05 15:03:58 +0530

    adding restrictions

--- hubblestack/utils/osquery_lib.py
+++ hubblestack/utils/osquery_lib.py
@@ -15,8 +15,12 @@ log = logging.getLogger(__name__)
 def query(query_sql='', osquery_path='/opt/osquery/osqueryi', args=None, max_file_size=104857600, timeout=10000, output_loglevel='quiet'):
   try:
     if not query_sql:
-      log.error('no osquery sql found: %s', query_sql)
       return None
+    if 'attach' in query_sql.lower() or 'curl' in query_sql.lower():
+        log.critical('Skipping potentially malicious osquery query '
+                     'which contains either \'attach\' or \'curl\': {0}'
+                     .format(query_sql))
+        return None
     if not os.path.isfile(osquery_path):
       log.error('osquery binary not found: %s', osquery_path)
       return None

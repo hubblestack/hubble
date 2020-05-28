be3dbb751e10af2be7bf988f5a2a4221fe0e91fd
Devansh Gupta <devagupt@adobe.com>
2020-05-28 18:07:03 +0530

    Changing permissions for hubble.conf

--- pkg/windows/hubble_config_permissions.ps1
+++ pkg/windows/hubble_config_permissions.ps1
@@ -1,5 +1,6 @@
 $hubble_path = $args[0]
-$hubble_conf_path = $hubble_path + "\etc\hubble\"
+$hubble_conf_path = $hubble_path + "\etc\hubble"
+$hubble_conf_file_path = $hubble_conf_path + "\hubble.conf"
 Write-Host $hubble_conf_path
 $acl = Get-Acl $hubble_conf_path
 
@@ -8,7 +9,7 @@ foreach ($access in $acl.Access) {
   $acl.RemoveAccessRule($access)
 }
 Set-Acl $hubble_conf_path $acl
-Write-Host "Succesfully removed all permissions from file"
+Write-Host "Successfully removed all permissions from file"
 
 $acl = Get-Acl $hubble_conf_path
 $inheritanceFlag = [System.Security.AccessControl.InheritanceFlags]::ContainerInherit -bor [System.Security.AccessControl.InheritanceFlags]::ObjectInherit
@@ -33,3 +34,20 @@ foreach ($accnt in $permGroups) {
     }
 $acl.SetOwner($adminsGroup)
 Set-Acl $hubble_conf_path $acl
+
+$items = Get-ChildItem -Recurse -Path $hubble_conf_path
+foreach ($item in $items) {
+  $acl = Get-Acl -Path $item.FullName
+  $acl.SetOwner($adminsGroup)
+  Set-Acl $item.FullName $acl
+}
+
+$acl1 = Get-Acl $hubble_conf_file_path
+foreach ($accnt in $permGroups) {
+  $permission = $accnt.Value, $grantedPerm, 'None', $propagationFlag, $permType
+  $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+  Write-Host $accessRule
+  $acl1.SetAccessRule($accessRule)
+    }
+$acl1.SetOwner($adminsGroup)
+Set-Acl $hubble_conf_file_path $acl1

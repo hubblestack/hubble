28e459ddca0da3c3e8d3a4803aecdb817bfecb9c
Paul Miller <paul@jettero.pl>
2020-07-06 10:46:35 -0400

    add some dostrings to better explain test purpose

--- tests/unittests/test_loader.py
+++ tests/unittests/test_loader.py
@@ -14,10 +14,17 @@ def module_dirs():
     return md
 
 def test_module_dirs(module_dirs):
+    """ interrogate module_dirs() to make sure none of its entries contain any
+        paths that might result in us accidentally loading salt modules from
+        some unreleated salt installation
+    """
     for item in module_dirs:
         assert 'var/cache/salt' not in item
 
 def test_can_find_uniquely_hubblestack_module(module_dirs):
+    """ test to make sure we can find specifically hubblestack modules in at
+        least one of the module_dirs()
+    """
     found = False
     for path in module_dirs:
         if os.path.isfile( os.path.join(path, 'pulsar.py') ):
@@ -27,6 +34,11 @@ def test_can_find_uniquely_hubblestack_module(module_dirs):
 
 # XXX: we'll want to remove this test when we stop looking for installed saltstack
 def test_can_find_uniquely_saltstack_module(module_dirs):
+    """ check to make sure that we can still find salt modules somewhere in module_dirs()
+        ... although we'll want to remove this test eventually, around about
+        the time we remove the ability to load saltstack modules from salt
+        installs.
+    """
     found = False
     for path in module_dirs:
         if os.path.isfile( os.path.join(path, 'saltutil.py') ):

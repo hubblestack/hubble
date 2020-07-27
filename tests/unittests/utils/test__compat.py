874fa684f31bb9a3ec08e15779ee1a505ace8aa7
Gorav Singal <gorav.singal@gmail.com>
2020-07-27 10:46:19 +0530

    Final set of test cases for Grains dependencies
    - No test cases for
      salt.utils.user, salt.platform.win, salt.utils.win_runas,
      salt.utils.dns, salt.utils.pkg.rpm, salt.modules.smbios,
      salt.utils.odict,
    - Moved test cases for:
      - salt.utils.win_functions
      - salt.utils.network
      - salt._compat
      - salt.utils.win_osinfo
      - salt.utils.win_reg
      - salt.utils.dictupdate
      - salt.utils.zfs
      - salt.utils.yaml/salt.utils.yamlloader

---
+++ tests/unittests/utils/test__compat.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+Unit tests for hubblestack.utils._compat
+'''
+
+# Import Python libs
+from __future__ import absolute_import, print_function, unicode_literals
+import logging
+import sys
+
+from tests.support.unit import TestCase
+
+import hubblestack.utils._compat as compat
+
+
+log = logging.getLogger(__name__)
+PY3 = sys.version_info.major == 3
+
+
+class CompatTestCase(TestCase):
+    def test_ipv6_class__is_packed_binary(self):
+        ipv6 = compat.IPv6AddressScoped('2001:db8::')
+        self.assertEqual(str(ipv6), '2001:db8::')
+
+    def test_ipv6_class__is_packed_binary_integer(self):
+        ipv6 = compat.IPv6AddressScoped(42540766411282592856903984951653826560)
+        self.assertEqual(str(ipv6), '2001:db8::')
+
+    def test_ipv6_class__is_packed_binary__issue_51831(self):
+        ipv6 = compat.IPv6AddressScoped(b'sixteen.digit.bn')
+        self.assertEqual(str(ipv6), '7369:7874:6565:6e2e:6469:6769:742e:626e')

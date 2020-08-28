e94d35f21057e1dbc8977b60c81cc684f3eb1938
Paul Miller <paul@jettero.pl>
2020-08-28 13:39:35 +0000

    change the load order and remove old path infos from daemon

--- hubblestack/log/__init__.py
+++ hubblestack/log/__init__.py
@@ -5,13 +5,9 @@ from hubblestack.log.setup import (
     emit_to_splunk,
     setup_splunk_logger,
     setup_console_logger,
-<<<<<<< HEAD
     setup_file_logger,
     filter_logs,
     is_logfile_configured,
     is_console_configured,
     is_logging_configured
-=======
-    setup_file_logger
->>>>>>> bc1e9e4... move hubblestack.splunklogging to hubblestack.log.splunk (and other related fixes)
 )

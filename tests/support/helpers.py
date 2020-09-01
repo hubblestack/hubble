2a15d64485f9223bd3aac2200041dd4be28dcad1
Mudit Agarwal <muditagr90@gmail.com>
2020-09-01 11:20:46 +0530

    making win_reg module salt-less (#909)
    * making win_reg module salt-less
    
    * removing salt reference from test file
    
    * removing more references

--- tests/support/helpers.py
+++ tests/support/helpers.py
@@ -1348,3 +1348,32 @@ def dedent(text, linesep=os.linesep):
     if not isinstance(text, str):
         return hubblestack.utils.stringutils.to_bytes(clean_text)
     return clean_text
+
+def random_string(prefix, size=6, uppercase=True, lowercase=True, digits=True):
+    """
+    Generates a random string.
+
+    ..versionadded: 3001
+
+    Args:
+        prefix(str): The prefix for the random string
+        size(int): The size of the random string
+        uppercase(bool): If true, include uppercased ascii chars in choice sample
+        lowercase(bool): If true, include lowercased ascii chars in choice sample
+        digits(bool): If true, include digits in choice sample
+    Returns:
+        str: The random string
+    """
+    if not any([uppercase, lowercase, digits]):
+        raise RuntimeError(
+            "At least one of 'uppercase', 'lowercase' or 'digits' needs to be true"
+        )
+    choices = []
+    if uppercase:
+        choices.extend(string.ascii_uppercase)
+    if lowercase:
+        choices.extend(string.ascii_lowercase)
+    if digits:
+        choices.extend(string.digits)
+
+    return prefix + "".join(random.choice(choices) for _ in range(size))

22d557e5766493a5e591b78a79bb11e7d0ea49ff
Chandler Newby <chandler.newby@gmail.com>
2020-09-16 10:56:15 -0600

    dict.update returns None

--- hubblestack/splunklogging.py
+++ hubblestack/splunklogging.py
@@ -170,7 +170,7 @@ class SplunkHandler(logging.Handler):
         grains and other values that were updated to be updated here.
         '''
         for entry in self.endpoint_list:
-            entry[1] = entry[1].update(hubblestack.utils.stdrec.std_info())
+            entry[1].update(hubblestack.utils.stdrec.std_info())
 
     def format_record(self, record):
         '''

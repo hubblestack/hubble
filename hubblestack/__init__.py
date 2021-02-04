bb4095585da37358796cd31f2ff4a76bf116abeb
Paul Miller <paul@jettero.pl>
2021-02-04 10:12:06 -0500

    cryptography needn't warn us py2 is deprecated (stfu)

--- hubblestack/__init__.py
+++ hubblestack/__init__.py
@@ -1,3 +1,8 @@
 __version__ = '3.0.14'
 
 __buildinfo__ = { 'branch' : 'BRANCH_NOT_SET' , 'last_commit' : 'COMMIT_NOT_SET' }
+
+
+import warnings
+warnings.filterwarnings('ignore', message='Python 2.*cryptography')
+# omigosh, py2 is deprecated?!?!? HOW DID I MISS THAT. (stfu)

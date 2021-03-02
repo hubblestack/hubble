b78837460de9ca805d489c0c7075b804fc0c792a
Gorav Singal <gsingal@adobe.com>
2021-03-02 13:49:13 +0530

    FIPS changes with Python 3.9
    FIPS changes with Python 3.9

--- pkg/centos7/entrypoint.sh
+++ pkg/centos7/entrypoint.sh
@@ -24,11 +24,8 @@ cp /hubble_build/hubblestack/__init__.py /hubble_build/hubblestack/__init__.fixe
 
 sed -i -e "s/'.*'/'$HUBBLE_VERSION_ENV'/g" /hubble_build/hubblestack/version.py
 
-eval "$(pyenv init -)"
-
 # locate some pyenv things
-pyenv_prefix="$(pyenv prefix)"
-python_binary="$(pyenv which python)"
+python_binary="$(which python)"
 while [ -L "$python_binary" ]
 do python_binary="$(readlink -f "$python_binary")"
 done
@@ -85,7 +82,7 @@ pip freeze > /data/requirements.txt
 [ -f ${_HOOK_DIR:-./pkg}/hook-hubblestack.py ] || exit 1
 
 rm -rf build dist /opt/hubble/hubble-libs /hubble_build/hubble.spec
-export LD_LIBRARY_PATH=$pyenv_prefix/lib:/opt/hubble/lib:/opt/hubble-libs
+export LD_LIBRARY_PATH=/opt/hubble/lib:/opt/hubble-libs
 export LD_RUN_PATH=$LD_LIBRARY_PATH
 pyinstaller --onedir --noconfirm --log-level ${_BINARY_LOG_LEVEL:-INFO} \
     --additional-hooks-dir ${_HOOK_DIR:-./pkg} \

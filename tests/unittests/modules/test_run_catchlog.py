3fc0e4a22018aa692888287cd5a96529bd7f01f0
Paul Miller <paul@jettero.pl>
2021-10-21 12:43:02 -0400

    use the __mods__ pytest fixture

--- tests/unittests/modules/test_run_catchlog.py
+++ tests/unittests/modules/test_run_catchlog.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from hubblestack.modules import run_catchlog
-from hubblestack.modules import safecommand
 from hubblestack.utils.platform import is_windows
 from hubblestack.utils.user import get_user
 from hubblestack.utils.files import remove
 
 
-def test_run():
+def test_run(__mods__):
     """
     test return of whoami in both safecommand module and run_catchlog
     """
 
     if not is_windows():
         usr = get_user()
-        cmd_results = safecommand.run("whoami")
-        safe_catchlog = run_catchlog.run("whoami")
+        cmd_results = __mods__["safecommand.run"]("whoami")
+        safe_catchlog = __mods__["run_catchlog.run"]("whoami")
         assert usr == safe_catchlog == cmd_results
 
 
-def test_catchlog():
+def test_catchlog(__mods__):
     """
     Ensure that run picks up error file as well as search string
     """
 
     with open("testing.txt", "w") as fh:
         fh.writelines("err blah testing")
-    ret = run_catchlog.run(file_check="testing.txt", search="err")
+    ret = __mods__["run_catchlog.run"](file_check="testing.txt", search="err")
     assert ret["events"] == ["[file_error_line]: err blah testing"]
     remove("testing.txt")

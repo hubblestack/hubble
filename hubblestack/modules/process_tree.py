4cd66c49d03200bc881339351348df0f225b5ca4
Paul Miller <paul@jettero.pl>
2021-10-22 19:31:23 -0400

    turn process_tree inside out. instead of a list of children, it now shows a list of ancestors

--- hubblestack/modules/process_tree.py
+++ hubblestack/modules/process_tree.py
@@ -10,14 +10,13 @@ SOURCETYPE = "hubble_process_tree"
 
 
 def __virtual__():
-    process_status.__mods__ = __mods__
     return __virtual_name__
 
 
 def _get_processes():
     query = (
         "SELECT main.pid ppid, main.name pname, kids.pid pid, kids.name name "
-        "FROM processes as main join processes as kids on kids.parent = main.pid "
+        "FROM processes AS main JOIN processes AS kids ON kids.parent = main.pid "
         "ORDER BY ppid"
     )
     res = process_status._run_query(query)
@@ -34,27 +33,20 @@ def tree(sourcetype=SOURCETYPE):
     the processes.
     `children` is a list of dicts containing the fields `name` and `pid`"""
     processes = _get_processes()
-    now = int(time.time())
     # sanity check
     if not processes:
         return None
-    events, prev_children = [], []
-    prev_pid = 0
-    prev_name = ""
-    for process in processes:
-        pid = process["ppid"]
-        if pid != prev_pid:
-            if prev_pid != 0:
-                # finished collecting the children of the process with pid 'prev_pid'
-                events.append({"pid": prev_pid, "name": prev_name, "children": prev_children})
-            # reset
-            prev_pid = pid
-            prev_name = process["pname"]
-            prev_children = []
-        prev_children.append({"pid": process["pid"], "name": process["name"]})
-    if prev_pid != 0:
-        events.append({"pid": prev_pid, "name": prev_name, "children": prev_children})
 
-    if events:
-        return {"time": now, "sourcetype": sourcetype, "events": events}
-    return None
+    processes = {p["pid"]: p for p in processes}
+
+    def ancestors(pid):
+        ret = list()
+        tmp = processes.get(pid)
+        while tmp is not None:
+            ret.append({"pid": tmp["pid"], "name": tmp["name"]})
+            tmp = processes.get(tmp["ppid"])
+        return ret
+
+    now = int(time.time())
+    events = [{"pid": p["pid"], "name": p["name"], "ancestors": ancestors(p["pid"])} for p in processes.values()]
+    return {"time": now, "sourcetype": sourcetype, "events": events}

9113e38962d64ba5fda9e56e3e1d6c7904e2d41e
Vlad Monea <monea@adobe.com>
2022-08-09 14:41:23 +0300

    remove unused imports

--- hubblestack/utils/signing.py
+++ hubblestack/utils/signing.py
@@ -61,7 +61,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding, utils
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import load_pem_private_key, Encoding, PrivateFormat, NoEncryption
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
 MANIFEST_RE = re.compile(r"^\s*(?P<digest>[0-9a-fA-F]+)\s+(?P<fname>.+)$")
 log = logging.getLogger(__name__)
